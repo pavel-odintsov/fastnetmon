eaea543ed16be9e5ca0eec441937bc1f232a15bd
vps2fast <odintsov@fastvps.ru>
2013-10-18 17:28:00 +0400

    New system:)

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ENGINE=ULOG2
-ENGINE=PCAP
+#ENGINE=PCAP
 
 g++ libipulog.c -c -o libipulog.o -Wno-write-strings
 g++ -D$ENGINE fastnetmon.cpp libipulog.o -lpcap -o fastnetmon
--- notify_about_attack.sh
+++ notify_about_attack.sh
@@ -5,7 +5,6 @@
 #$3 pps_as_string
 
 email_notify="odintsov@fastvps.ru,hohryakov@fastvps.ru,ziltsov@fastvps.ee"
-
-echo "Please execute reglaments and notify client" | mail -s "Myflower Guard: IP $1 blocked bacause $2 attack with power $3 pps" $email_notify
+echo "Subject, please execute all related tasks :)" | mail -s "Myflower Guard: IP $1 blocked bacause $2 attack with power $3 pps" $email_notify
 
 
