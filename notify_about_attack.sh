bd7a52528b52a67ef80ab5e34d357c1dbc5b008c
Pavel Odintsov <pavel.odintsov@gmail.com>
2014-10-21 11:20:18 +0400

    Fixed #31 Add automatic unban after timeout

--- notify_about_attack.sh
+++ notify_about_attack.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-#$1 client_ip_as_string
-#$2 data_direction
-#$3 pps_as_string
+# $1 client_ip_as_string
+# $2 data_direction
+# $3 pps_as_string
+# $4 action (ban or unban)
 
 email_notify="root,please_fix_this_email@domain.ru"
 
 # Далее возможны два варианта:
 # это первый запуск, при котором нужно банить IP (на stdin пусто)
 # это второй запуск, когда скрипт уже собрал (если смог) детали об атаке (на stdin даные об атаке)
+
+if [ "$4" = "unban" ]; then
+    # Unban actions if used
+    exit 0
+fi
 
 # check stdin type
 if [ -t 0 ]; then
@@ -17,5 +23,5 @@ if [ -t 0 ]; then
     # iptables -A INPUT -s $1 -j DROP
     # iptables -A INPUT -d $1 -j DROP
 else
-    cat | mail -s "Myflower Guard: IP $1 blocked because $2 attack with power $3 pps" $email_notify;
+    cat | mail -s "FastNetMon Guard: IP $1 blocked because $2 attack with power $3 pps" $email_notify;
 fi
