ba2f5e8a274cd21bc51135075b6c2dbad784937a
vps2fast <odintsov@fastvps.ru>
2014-10-22 22:33:00 +0400

    Enhance performance test facility

--- performance_tests.cpp
+++ performance_tests.cpp
@@ -8,6 +8,9 @@
 #include <boost/thread.hpp>
 #include <boost/thread/mutex.hpp>
 
+// apt-get install -y libtbb-dev 
+#include "tbb/concurrent_unordered_map.h"
+
 typedef struct {
     unsigned  int in_bytes;
     unsigned  int out_bytes;
@@ -32,6 +35,7 @@ std::map <uint32_t, map_element> DataCounter;
 boost::mutex data_counter_mutex;
 
 std::unordered_map <uint32_t, map_element> DataCounterUnordered;
+tbb::concurrent_unordered_map <uint32_t, map_element> DataCounterUnorderedConcurrent;
 
 using namespace std;
 
@@ -41,7 +45,6 @@ int number_of_retries = 100;
 // 83 seconds
 // without mutexes segmentation fault
 void packet_collector_thread_std_map() {
-    std::cout<<"Start thread"<<endl;
     for (int iteration = 0; iteration < number_of_retries; iteration++) {
         for(uint32_t i = 0; i < number_of_ips; i++) {
             data_counter_mutex.lock();
@@ -54,7 +57,6 @@ void packet_collector_thread_std_map() {
 // 52 seconds
 // without mutexes segmentation fault
 void packet_collector_thread_unordered_map() {
-    std::cout<<"Start thread"<<endl;
     for (int iteration = 0; iteration < number_of_retries; iteration++) {
         for(uint32_t i = 0; i < number_of_ips; i++) {
             data_counter_mutex.lock();
@@ -64,15 +66,22 @@ void packet_collector_thread_unordered_map() {
     }   
 }
 
-int main() {
+void packet_collector_thread_unordered_concurrent_map() {
+    for (int iteration = 0; iteration < number_of_retries; iteration++) {
+        for(uint32_t i = 0; i < number_of_ips; i++) {
+            DataCounterUnorderedConcurrent[i].udp_in_bytes++;
+        }   
+    }   
+}
+
+int run_tests(void (*tested_function)(void)) {
     timespec start_time;
     clock_gettime(CLOCK_REALTIME, &start_time);
 
     std::cout<<"Run threads"<<endl;
     boost::thread* threads[8];
     for (int i = 0; i < 9; i++) {
-        cout<<"Run thread: "<<i<<endl;
-        threads[i] = new boost::thread(packet_collector_thread_std_map);
+        threads[i] = new boost::thread(tested_function);
     }
 
     std::cout<<"All threads started"<<endl;
@@ -86,5 +95,17 @@ int main() {
     timespec finish_time;
     clock_gettime(CLOCK_REALTIME, &finish_time);
 
+    double total_operations = number_of_ips*number_of_retries*8;
     std::cout<<"Seconds: "<<finish_time.tv_sec - start_time.tv_sec<<std::endl;
+    std::cout<<"Operations per second: "<<int(total_operations/(finish_time.tv_sec - start_time.tv_sec))<<endl;
+}
+
+int main() {
+    std::cout<<"Standard map"<<endl;
+    run_tests(packet_collector_thread_std_map);
+    std::cout<<"Standard unordered map from C++11"<<endl;
+    run_tests(packet_collector_thread_unordered_map);
+    std::cout<<"Standard unordered concurrent map from Intel TBB"<<endl; 
+    run_tests(packet_collector_thread_unordered_concurrent_map);
+
 }
