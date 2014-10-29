f48b6844a811e96cd703908899949266fb30bde0
Pavel Odintsov <pavel.odintsov@gmail.com>
2014-10-29 15:02:11 +0400

    Add test with netmap

---
+++ netmap.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#define NETMAP_WITH_LIBS
+#include <net/netmap_user.h>
+
+int main() {
+
+}
+
+
+struct nm_desc base_nmd;
+
+/*
+uint64_t nmd_flags;
+struct nm_desc *nmd;
+
+
+bzero(&base_nmd, sizeof(base_nmd));
+
+
+// Open the netmap device using nm_open()
+// protocol stack and may cause a reset of the card
+nmd = nm_open("eth3", NULL, , &base_nmd);
+
+if (nmd == NULL) {
+    printf("Unable to open netmap");
+    exit(1);
+}
+
+
+*/
