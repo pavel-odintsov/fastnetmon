c8f89baa4a3689c237c9334698504044e2061481
Pavel Odintsov <pavel.odintsov@gmail.com>
2015-01-26 11:38:16 +0300

    Complete working solution for testing plugins

--- build_plugin_tester.sh
+++ build_plugin_tester.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 g++ netflow_plugin/netflow_collector.cpp -c -onetflow_collector.o
-g++ plugin_runner.cpp  -llog4cpp netflow_collector.o -oplugin_tester
+g++ sflow_plugin/sflow_collector.cpp -c -osflow_collector.o
+g++ plugin_runner.cpp  -llog4cpp netflow_collector.o sflow_collector.o -oplugin_tester
 
--- plugin_runner.cpp
+++ plugin_runner.cpp
@@ -2,6 +2,7 @@
 #include <sys/types.h>
 #include <stdio.h>
 #include <string.h>
+#include <iterator>
 #include <sstream>
 #include <netinet/ip.h>
 
@@ -11,6 +12,7 @@
 
 #include "fastnetmon_types.h"
 #include "netflow_plugin/netflow_collector.h"
+#include "sflow_plugin/sflow_collector.h"
 
 // log4cpp logging facility
 #include "log4cpp/Category.hh"
@@ -80,6 +82,77 @@ string convert_ip_as_uint_to_string(uint32_t ip_as_integer) {
     return (string)inet_ntoa(ip_addr);
 }
 
+// http://stackoverflow.com/questions/14528233/bit-masking-in-c-how-to-get-first-bit-of-a-byte
+int extract_bit_value(uint8_t num, int bit) {
+    if (bit > 0 && bit <= 8) {
+        return ( (num >> (bit-1)) & 1 );
+    } else {
+        return 0;
+    }
+}
+
+string print_tcp_flags(uint8_t flag_value) {
+    if (flag_value == 0) {
+        return "-";
+    }
+
+    // cod from pfring.h
+    // (tcp->fin * TH_FIN_MULTIPLIER) + (tcp->syn * TH_SYN_MULTIPLIER) +
+    // (tcp->rst * TH_RST_MULTIPLIER) + (tcp->psh * TH_PUSH_MULTIPLIER) +
+    // (tcp->ack * TH_ACK_MULTIPLIER) + (tcp->urg * TH_URG_MULTIPLIER);
+
+    /*
+        // Required for decoding tcp flags
+        #define TH_FIN_MULTIPLIER   0x01
+        #define TH_SYN_MULTIPLIER   0x02
+        #define TH_RST_MULTIPLIER   0x04
+        #define TH_PUSH_MULTIPLIER  0x08
+        #define TH_ACK_MULTIPLIER   0x10
+        #define TH_URG_MULTIPLIER   0x20
+    */
+
+    vector<string> all_flags;
+
+    if (extract_bit_value(flag_value, 1)) {
+        all_flags.push_back("fin");
+    }
+    
+    if (extract_bit_value(flag_value, 2)) {
+        all_flags.push_back("syn");
+    }   
+
+    if (extract_bit_value(flag_value, 3)) {
+        all_flags.push_back("rst");
+    }   
+
+    if (extract_bit_value(flag_value, 4)) {
+        all_flags.push_back("psh");
+    }   
+
+    if (extract_bit_value(flag_value, 5)) {
+        all_flags.push_back("ack");
+    }    
+
+    if (extract_bit_value(flag_value, 6)) {
+        all_flags.push_back("urg");
+    }   
+
+    
+    ostringstream flags_as_string;
+
+    if (all_flags.empty()) {
+        return "-";
+    }
+
+    // concatenate all vector elements with comma
+    std::copy(all_flags.begin(), all_flags.end() - 1, std::ostream_iterator<string>(flags_as_string, ","));
+
+    // add last element
+    flags_as_string << all_flags.back();
+    
+    return flags_as_string.str();
+}
+
 string print_simple_packet(simple_packet packet) {
     std::stringstream buffer;
 
@@ -92,9 +165,9 @@ string print_simple_packet(simple_packet packet) {
         <<" protocol: "<<get_printable_protocol_name(packet.protocol);
    
     // Print flags only for TCP 
-    //if (packet.protocol == IPPROTO_TCP) { 
-    //    buffer<<" flags: "<<print_tcp_flags(packet.flags);
-    //}
+    if (packet.protocol == IPPROTO_TCP) { 
+        buffer<<" flags: "<<print_tcp_flags(packet.flags);
+    }
 
     buffer<<" size: "<<packet.length<<" bytes"<<"\n";
     
@@ -105,10 +178,23 @@ void process_packet(simple_packet& current_packet) {
     std::cout<<print_simple_packet(current_packet);
 }
 
-int main() {
+int main(int argc, char *argv[]) {
     init_logging(); 
     
-    start_netflow_collection(process_packet);
+    if (argc < 2) {
+        std::cout<<"Please specify sflow or netflow as param";
+        return 1;
+    }
+
+    if (strstr(argv[1], "sflow") != NULL) {
+        std::cout<<"Starting sflow"<<std::endl;
+        start_sflow_collection(process_packet);
+    } else if (strstr(argv[1], "netflow") != NULL) {
+        std::cout<<"Starting netflow"<<std::endl;
+        start_netflow_collection(process_packet);
+    } else {
+        std::cout<<"Bad plugin name!";
+    }
 }
 
 
