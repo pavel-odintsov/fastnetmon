0fcf3ff1b0f052553e0f01ee7949665676d7f306
Pavel Odintsov <pavel.odintsov@gmail.com>
2015-01-27 12:56:16 +0300

    We implemented complete PCAP support in separate plugin. Please be aware! It's very slow! Closes #100

--- build_plugin_tester.sh
+++ build_plugin_tester.sh
@@ -2,5 +2,7 @@
 
 g++ netflow_plugin/netflow_collector.cpp -c -onetflow_collector.o
 g++ sflow_plugin/sflow_collector.cpp -c -osflow_collector.o
-g++ plugin_runner.cpp  -llog4cpp netflow_collector.o sflow_collector.o -oplugin_tester
+g++ pcap_plugin/pcap_collector.cpp -c  -opcap_collector.o
+g++ plugin_runner.cpp -lpcap -llog4cpp netflow_collector.o sflow_collector.o pcap_collector.o -oplugin_tester
+rm -f netflow_collector.o sflow_collector.o pcap_collector.o
 
