4fa8fd79e66c85e86e2447ac3bb5ed345dcf520c
Pavel Odintsov <pavel.odintsov@gmail.com>
2015-02-12 16:04:04 +0300

    Working example for netmap on FreeBSD

--- tests/netmap.cpp
+++ tests/netmap.cpp
@@ -1,30 +1,48 @@
 #include <stdio.h>
+#include <iostream>
+#include <string>
 #define NETMAP_WITH_LIBS
 #include <net/netmap_user.h>
 
+// For pooling operations
+#include <poll.h>
+
+// Compilation on FreeBSD 10: clang++ netmap.cpp
+
+void consume_pkt(u_char* buffer, int len) {
+    printf("Got packet with length: %d", len);
+}
+
+void receiver(void) {
+    struct  nm_desc *netmap_descriptor;
+    struct  pollfd fds;
+    struct  nm_pkthdr h;
+    u_char* buf;
+
+    std::string interface = "netmap:em0"; 
+    netmap_descriptor = nm_open(interface.c_str(), NULL, 0, 0);
+
+    if (netmap_descriptor == NULL) {
+        printf("Can't open netmap device %s", interface.c_str());
+        return;
+    }
+
+
+    fds.fd  = NETMAP_FD(netmap_descriptor);
+    fds.events = POLLIN;
+
+    for (;;) {
+        poll(&fds,  1, -1);
+        
+        while ( (buf = nm_nextpkt(netmap_descriptor, &h)) )
+            consume_pkt(buf, h.len);
+        }
+
+     nm_close(netmap_descriptor);
+}
+
 int main() {
-
+    printf("Hello\n");
+    receiver();
 }
 
-
-struct nm_desc base_nmd;
-
-/*
-uint64_t nmd_flags;
-struct nm_desc *nmd;
-
-
-bzero(&base_nmd, sizeof(base_nmd));
-
-
-// Open the netmap device using nm_open()
-// protocol stack and may cause a reset of the card
-nmd = nm_open("eth3", NULL, , &base_nmd);
-
-if (nmd == NULL) {
-    printf("Unable to open netmap");
-    exit(1);
-}
-
-
-*/
