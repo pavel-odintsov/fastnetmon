80499b30d783d310de55f804b6a066f8a9d84f1d
Pavel Odintsov <pavel.odintsov@gmail.com>
2015-02-26 16:26:51 +0300

    Fix syn umbrella code for PFRING 6.0.2 and 6.0.3 support both

--- build_syn_umbrella.sh
+++ build_syn_umbrella.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-g++ syn_umbrella.cpp -I/opt/crafter/include -I/opt/pf_ring/include -lpthread -l/opt/pf_ring/lib/libpfring.so -lnuma -l/opt/crafter/lib/libcrafter.so -o umbrella 
+g++ syn_umbrella.cpp -I/opt/crafter/include -I/opt/pf_ring/include -lpthread -l/opt/pf_ring/lib/libpfring.so -lnuma -l/opt/crafter/lib/libcrafter.so -o synumbrella
+
+# ./umbrella -i zc:eth4 -c 1 -o zc:eth4 -g 0 -v 
