cb4f79df6b089a623acf31d7c10475667ae421c5
Pavel Odintsov <pavel.odintsov@gmail.com>
2015-03-06 13:00:09 +0300

    Rename files with packet parser

--- tests/build_netmap.bash
+++ tests/build_netmap.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-clang++ pf_ring_packet_parser.cpp -c -opf_ring_packet_parser.o
-g++ netmap.cpp -I/usr/src/fastnetmon/tests/netmap_includes -lboost_thread -lboost_system pf_ring_packet_parser.o
+clang++ fastnetmon_packet_parser.cpp -c -ofastnetmon_packet_parser.o
+g++ netmap.cpp -I/usr/src/fastnetmon/tests/netmap_includes -lboost_thread -lboost_system fastnetmon_packet_parser.o
 
