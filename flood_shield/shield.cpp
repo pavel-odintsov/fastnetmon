e2e326092be59c7b870bf424d06411d2adf52864
Pavel Odintsov <pavel.odintsov@gmail.com>
2015-03-12 13:32:48 +0300

    Port from pcap to PF_RING because pcap is non adequately load server:
    pcap performance:
    
    top - 12:36:20 up 7 days, 3 min,  2 users,  load average: 0.01, 0.02, 0.05
    Tasks:  76 total,   2 running,  74 sleeping,   0 stopped,   0 zombie
    %Cpu(s):  0.9 us,  1.1 sy,  0.0 ni, 97.3 id,  0.0 wa,  0.0 hi,  0.7 si,  0.0 st
    KiB Mem:  32980328 total,  1151816 used, 31828512 free,   122020 buffers
    KiB Swap:  8387580 total,        0 used,  8387580 free,   423452 cached
      PID USER      PR  NI  VIRT  RES  SHR S  %CPU %MEM    TIME+  COMMAND
    21505 root      20   0 32492 1012  844 R   3.7  0.0   0:00.79 shield
    21269 www-data  20   0  297m 3656 1212 S   2.7  0.0   0:07.56 apache2
    21268 www-data  20   0  297m 3640 1196 S   2.0  0.0   0:07.56 apache2
    
    vanilla non zc pfring performance:
    top - 13:31:50 up 21 min,  2 users,  load average: 0.06, 0.06, 0.03
    Tasks:  76 total,   1 running,  75 sleeping,   0 stopped,   0 zombie
    %Cpu(s):  0.5 us,  0.6 sy,  0.0 ni, 97.8 id,  0.0 wa,  0.0 hi,  1.1 si,  0.0 st
    KiB Mem:  32980328 total,   387756 used, 32592572 free,    18872 buffers
    KiB Swap:  8387580 total,        0 used,  8387580 free,   204560 cached
    PID USER      PR  NI  VIRT  RES  SHR S  %CPU %MEM    TIME+  COMMAND
    2647 www-data  20   0  297m 3688 1236 S   2.3  0.0   0:02.79 apache2
    3466 root      20   0 23112 1588 1164 R   0.3  0.0   0:00.03 top
    3468 root      20   0 23024 1424 1144 S   0.3  0.0   0:00.02 shield

--- flood_shield/shield.cpp
+++ flood_shield/shield.cpp
@@ -1,12 +1,11 @@
-#include "../fastnetmon_packet_parser.h"
 #include "picohttpparser.h"
-#include <pcap.h>
+#include "pfring.h"
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
 
-void process_packet(u_char *user, struct pcap_pkthdr *packethdr, const u_char *packetptr);
+void parse_packet_pf_ring(const struct pfring_pkthdr *packet_header, const u_char *packetptr, const u_char *user_bytes);
 int shield();
 int extract_bit_value(uint8_t num, int bit);
 
@@ -24,28 +23,33 @@ int extract_bit_value(uint8_t num, int bit) {
 }
 
 int shield() {
-    char errbuf[PCAP_ERRBUF_SIZE];
-    pcap_t* descr = pcap_create("eth4", errbuf);
+    unsigned int snaplen = 1500;
+    u_int8_t wait_for_packet = 1;
+    u_int32_t flags = 0;
+   
+    flags |= PF_RING_PROMISC;
+    flags |= PF_RING_DO_NOT_PARSE; 
+    
+    pfring* pf_ring_descr = pfring_open("eth4", snaplen, flags); 
 
-    if (descr == NULL) {
-        printf("Can't create pcap descriptor\n");
+    if (pf_ring_descr == NULL) {
+        printf("Can't create PF_RING descriptor: %s\n", strerror(errno));
         exit(1);
     }
 
-    unsigned int pcap_buffer_size_mbytes = 10;
-    int set_buffer_size_res = pcap_set_buffer_size(descr, pcap_buffer_size_mbytes * 1024 * 1024);
-
-    if (set_buffer_size_res != 0 ) {
-        printf("Can't set buffer size due to error: %s", set_buffer_size_res);
-        exit(1);
+    pfring_set_application_name(pf_ring_descr, (char*)"flood_shield");
+    
+    int pfring_set_socket_mode_result =  pfring_set_socket_mode(pf_ring_descr, recv_only_mode);
+    if (pfring_set_socket_mode_result != 0) {
+        printf("Function pfring_set_socket_mode failed\n");
     } 
 
-    if (pcap_activate(descr) != 0) {
-        printf("Call pcap_activate was failed\n");
+    if (pfring_enable_ring(pf_ring_descr) != 0) {
+        printf("Can't enable PF_RING\n");
         exit(1);
     }
 
-    pcap_loop(descr, -1, (pcap_handler)process_packet, NULL);
+    pfring_loop(pf_ring_descr, parse_packet_pf_ring, (u_char*)NULL, wait_for_packet);
 }
 
 int parse_http_request(const u_char* buf, int packet_len) {
@@ -64,12 +68,13 @@ int parse_http_request(const u_char* buf, int packet_len) {
     pret = phr_parse_request((const char*)buf, buflen, &method, &method_len, &path, &path_len, &minor_version, headers, &num_headers, prevbuflen);
 
     if (pret > 0) {
-        printf("We successfully parsed the request\n");
+        // printf("We successfully parsed the request\n");
     } else {
         printf("Parser failed\n");
         return 1;
     }
 
+    /*
     printf("request is %d bytes long\n", pret);
     printf("method is %.*s\n", (int)method_len, method);
     printf("path is %.*s\n", (int)path_len, path);
@@ -79,37 +84,34 @@ int parse_http_request(const u_char* buf, int packet_len) {
         printf("%.*s: %.*s\n", (int)headers[i].name_len, headers[i].name,
             (int)headers[i].value_len, headers[i].value);
     }
+    */
 
     return 0;
 }
 
-void process_packet(u_char *user, struct pcap_pkthdr *packethdr, const u_char *packetptr) {
-    struct pfring_pkthdr packet_header;
-    memset(&packet_header, 0, sizeof(packet_header));
-    packet_header.len = packethdr->len;
-    packet_header.caplen = packethdr->caplen;
-
-    fastnetmon_parse_pkt((u_char*)packetptr, &packet_header, 4, 1, 0);
+void parse_packet_pf_ring(const struct pfring_pkthdr *packet_header, const u_char *packetptr, const u_char *user_bytes) {
+    memset((void*)&packet_header->extended_hdr.parsed_pkt, 0, sizeof(packet_header->extended_hdr.parsed_pkt));
+    pfring_parse_pkt((u_char*)packetptr, (struct pfring_pkthdr*)packet_header, 4, 1, 0);
     
     // Ignore tcp handshake requests
-    if (extract_bit_value(packet_header.extended_hdr.parsed_pkt.tcp.flags, 2) or // SYN
-        extract_bit_value(packet_header.extended_hdr.parsed_pkt.tcp.flags, 1)    // FIN
+    if (extract_bit_value(packet_header->extended_hdr.parsed_pkt.tcp.flags, 2) or // SYN
+        extract_bit_value(packet_header->extended_hdr.parsed_pkt.tcp.flags, 1)    // FIN
     ) {
         // printf("!!! skip syn/fin !!!\n");
         return;
     }
 
     // Skip zero length packets (also part of tcp/ip handshake)
-    if (packet_header.len == packet_header.extended_hdr.parsed_pkt.offset.payload_offset) {
+    if (packet_header->len == packet_header->extended_hdr.parsed_pkt.offset.payload_offset) {
         // printf("Skip zero length packet\n");
         return;
     }
 
     // We process only packets arrives at 80 port
     // TBD: add SNI support
-    if (packet_header.extended_hdr.parsed_pkt.l4_dst_port != 80) {
+    if (packet_header->extended_hdr.parsed_pkt.l4_dst_port != 80) {
         //char print_buffer[512];
-        //fastnetmon_print_parsed_pkt(print_buffer, 512, (u_char*)packetptr, &packet_header);
+        //pfring_print_parsed_pkt(print_buffer, 512, (u_char*)packetptr, &packet_header);
         //printf("%s", print_buffer);
         return;
     } 
@@ -117,11 +119,11 @@ void process_packet(u_char *user, struct pcap_pkthdr *packethdr, const u_char *p
     //printf("We got request to 80 port\n");
     //printf("payload shift: %d\n", packet_header.extended_hdr.parsed_pkt.offset.payload_offset);
 
-    int result = parse_http_request(packetptr + packet_header.extended_hdr.parsed_pkt.offset.payload_offset, packethdr->len);    
+    int result = parse_http_request(packetptr + packet_header->extended_hdr.parsed_pkt.offset.payload_offset, packet_header->len);    
     
     if (result != 0) {
         char print_buffer[512];
-        fastnetmon_print_parsed_pkt(print_buffer, 512, (u_char*)packetptr, &packet_header);
+        pfring_print_parsed_pkt(print_buffer, 512, (u_char*)packetptr, packet_header);
         printf("%s", print_buffer);
     }
 }
