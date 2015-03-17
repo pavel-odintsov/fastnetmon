d340d974af36eb4b082390cf64385629b63a5b7f
Pavel Odintsov <pavel.odintsov@gmail.com>
2015-03-17 12:14:49 +0300

    Add help to project remark

--- flood_shield/build_shield.sh
+++ flood_shield/build_shield.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# apt-get install -y ipset
+# apt-get install -y ipset apt-get install -y libipset-dev
 gcc picohttpparser.c -c -o picohttpparser.o
 g++ shield.cpp picohttpparser.o -opicohttpparser -oshield -I/opt/pf_ring/include -L/opt/pf_ring/lib -lpfring -lnuma -lpcap
 
