abc2543cbee80df4ffda639480a2485cb0940574
Pavel Odintsov <pavel.odintsov@gmail.com>
2015-03-18 19:16:39 +0300

    Flood shield moved to separate repo: https://github.com/FastVPSEestiOu/flood_shield

--- flood_shield/build_shield.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-g++ ipset_management.cpp -lipset -c -o ipset_management.o -I/opt/ipset/include -L/opt/ipset/lib
-gcc picohttpparser.c -c -o picohttpparser.o
-g++ shield.cpp picohttpparser.o ipset_management.o -opicohttpparser -oshield -I/opt/pf_ring/include -L/opt/pf_ring/lib -lpfring -lnuma -lpcap -lipset -L/opt/ipset/lib
