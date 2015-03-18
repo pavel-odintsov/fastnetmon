30e603c0fbbfaad0b97d482d7893ad5e5c932f2f
Pavel Odintsov <pavel.odintsov@gmail.com>
2015-03-18 16:30:01 +0300

    Switch to C++ for ipset; Fix varnings about char

--- flood_shield/build_ipset.sh
+++ flood_shield/build_ipset.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-gcc ipset.c  -I/opt/ipset/include/ -L/opt/ipset/lib -lipset
-# gcc ipset.c -lipset
+#gcc ipset.c  -I/opt/ipset/include/ -L/opt/ipset/lib -lipset
+g++ ipset.c -lipset
--- flood_shield/ipset.c
+++ flood_shield/ipset.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
 extern "C" {
     // #include <libipset/debug.h>             /* D() */
     #include <libipset/data.h>              /* enum ipset_data */
@@ -12,13 +11,13 @@ extern "C" {
     #include <libipset/utils.h>             /* STREQ */
 }
 
-int ban_ip(char* blacklist_name, char* ip_addr);
+int ban_ip(const char* blacklist_name, const char* ip_addr);
 
 int main() {
     ban_ip("blacklist", "10.10.10.1");
 }
 
-int ban_ip(char* blacklist_name, char* ip_addr) {
+int ban_ip(const char* blacklist_name, const char* ip_addr) {
     struct ipset_session *session = ipset_session_init(printf);
 
     if (session == NULL) {
@@ -41,7 +40,7 @@ int ban_ip(char* blacklist_name, char* ip_addr) {
     }
 
     printf("bool value:%d\n", type->last_elem_optional);
-    int ipset_parse_elem_ret = ipset_parse_elem(session, type->last_elem_optional, ip_addr);
+    int ipset_parse_elem_ret = ipset_parse_elem(session, (ipset_opt)type->last_elem_optional, ip_addr);
 
     if (ipset_parse_elem_ret < 0) {
         printf("Can't call ipset_parse_elem, error: %d\n", ipset_parse_elem_ret);
