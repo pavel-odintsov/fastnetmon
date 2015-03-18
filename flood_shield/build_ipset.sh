e9477d644113a0ef80e889bb5b2e6d52f8517d9c
Pavel Odintsov <pavel.odintsov@gmail.com>
2015-03-18 18:19:14 +0300

    Production ready Flood Shield

--- flood_shield/build_ipset.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-#g++ ipset.c  -I/opt/ipset/include/ -L/opt/ipset/lib -lipset
-g++ ipset.c -lipset
--- flood_shield/ipset_management.cpp
+++ flood_shield/ipset_management.cpp
@@ -1,5 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+
+#include "ipset_management.h"
 
 extern "C" {
     // #include <libipset/debug.h>             /* D() */
@@ -11,64 +14,76 @@ extern "C" {
     #include <libipset/utils.h>             /* STREQ */
 }
 
-int ban_ip(const char* blacklist_name, const char* ip_addr);
-
+/*
 int main() {
-    ban_ip("blacklist", "7.7.7.7");
+    manage_ip_ban("blacklist", "8.8.8.8", IPSET_BLOCK);
+    sleep(20);
+    manage_ip_ban("blacklist", "8.8.8.8", IPSET_UNBLOCK);
 }
+*/
 
-int ban_ip(const char* blacklist_name, const char* ip_addr) {
+int manage_ip_ban(const char* blacklist_name, const char* ip_addr, ipset_action action) {
     /* Load set types */
     ipset_load_types();
 
     struct ipset_session *session = ipset_session_init(printf);
 
     if (session == NULL) {
-        printf("Can't init session");
-        exit(1);
+        //printf("Can't init session");
+        return 1;
     }
 
     int ipset_parse_setname_ret = ipset_parse_setname(session, IPSET_SETNAME, blacklist_name);
 
     if (ipset_parse_setname_ret < 0) {
-        printf("Can't check name");
-        exit(1);
+        //printf("Can't check name");
+        return 2;
     }
 
-    const struct ipset_type *type = ipset_type_get(session, IPSET_CMD_ADD);
+    enum ipset_cmd command = IPSET_CMD_ADD;
+
+    if (action == IPSET_BLOCK) {
+        command = IPSET_CMD_ADD;
+    } else if (action == IPSET_UNBLOCK) {
+        command = IPSET_CMD_DEL;
+    } else {
+        // printf("Unexpected action");
+        return 3;
+    }
+
+    const struct ipset_type *type = ipset_type_get(session, command);
 
     if (type == NULL) {
-        printf("ipset type get failed: %s\n", ipset_session_error(session));
-        exit(1);
+        //printf("ipset type get failed: %s\n", ipset_session_error(session));
+        return 4;
     }
 
     // We should convert second argument to enum because bug in ipset, it fixed in upstream
     int ipset_parse_elem_ret = ipset_parse_elem(session, (ipset_opt)type->last_elem_optional, ip_addr);
 
     if (ipset_parse_elem_ret < 0) {
-        printf("Can't call ipset_parse_elem, error: %d\n", ipset_parse_elem_ret);
-        printf("Error: %s", ipset_session_error(session));
+        // printf("Can't call ipset_parse_elem, error: %d\n", ipset_parse_elem_ret);
+        // printf("Error: %s", ipset_session_error(session));
 
-        exit(1);
+        return 5;
     }
 
-    // 777 - line number
-    int ret = ipset_cmd(session, IPSET_CMD_ADD, 777);
+    // 777 - line number; Can be any
+    int ret = ipset_cmd(session, command, 777);
 
     if (ret < 0) {
-        printf("Can't ipset_cmd");
-
-        exit(1);
+        //printf("Can't ipset_cmd");
+        return 6;
     }
 
     int commit_ret = ipset_commit(session);
 
     if (commit_ret < 0) {
-        printf("ipset_commit failed\n");
-        exit(1);
+        //printf("ipset_commit failed\n");
+        return 7;
     }
 
-    printf("Executed correctly\n");
+    //printf("Executed correctly\n");
 
     ipset_session_fini(session);
 
