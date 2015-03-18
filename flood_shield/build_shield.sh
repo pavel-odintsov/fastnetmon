3848962c429ecd12e1603b2b6b839e40acdbd677
Pavel Odintsov <pavel.odintsov@gmail.com>
2015-03-18 18:44:53 +0300

    Fix for CentOS 6

--- flood_shield/build_shield.sh
+++ flood_shield/build_shield.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-g++ ipset_management.cpp -lipset -c -o ipset_management.o
+g++ ipset_management.cpp -lipset -c -o ipset_management.o -I/opt/ipset/includes -L/opt/ipset/lib
 gcc picohttpparser.c -c -o picohttpparser.o
-g++ shield.cpp picohttpparser.o ipset_management.o -opicohttpparser -oshield -I/opt/pf_ring/include -L/opt/pf_ring/lib -lpfring -lnuma -lpcap -lipset
-
+g++ shield.cpp picohttpparser.o ipset_management.o -opicohttpparser -oshield -I/opt/pf_ring/include -L/opt/pf_ring/lib -lpfring -lnuma -lpcap -lipset -L/opt/ipset/lib
