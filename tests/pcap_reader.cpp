86851952ec704d90bd0cd4845d817d12a8a84c7b
Pavel Odintsov <pavel.odintsov@gmail.com>
2015-03-19 18:38:18 +0300

    Working pcap reader

--- tests/pcap_reader.cpp
+++ tests/pcap_reader.cpp
@@ -3,18 +3,26 @@
 #include <pcap.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdint.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 
+/* It's prototype for moc testing of FastNetMon, it's very useful for netflow or direct packet parsers debug */
 
 /* 
    pcap dump format:
     global header: struct pcap_file_header
-    packet header: struct pcap_pkthdr
-
-
-
+    packet header: struct fastnetmon_pcap_pkthdr
 */
+
+// We can't use pcap_pkthdr from upstream because it uses 16 bytes timeval instead of 8 byte and broke everything
+struct fastnetmon_pcap_pkthdr {
+    uint32_t ts_sec;         /* timestamp seconds */
+    uint32_t ts_usec;        /* timestamp microseconds */
+    uint32_t incl_len;       /* number of octets of packet saved in file */
+    uint32_t orig_len;       /* actual length of packet */
+};
+
 int pcap_reader(const char* pcap_file_path) {
     int filedesc = open(pcap_file_path, O_RDONLY);
 
@@ -38,37 +46,33 @@ int pcap_reader(const char* pcap_file_path) {
         return -2;
     }
 
-    printf("Max length of captured packets for this file: %d bytes\n", pcap_header.snaplen);
-
     // Buffer for packets
     char packet_bufer[pcap_header.snaplen];
 
     unsigned int read_packets = 0; 
     while (1) {
-        struct pcap_pkthdr pcap_packet_header;
-        ssize_t packet_header_readed_bytes = read(filedesc, &pcap_packet_header, sizeof(struct pcap_pkthdr));
+        struct fastnetmon_pcap_pkthdr pcap_packet_header;
+        ssize_t packet_header_readed_bytes = read(filedesc, &pcap_packet_header, sizeof(struct fastnetmon_pcap_pkthdr));
       
-        if (packet_header_readed_bytes != sizeof(struct pcap_pkthdr)) {
+        if (packet_header_readed_bytes != sizeof(struct fastnetmon_pcap_pkthdr)) {
             // We haven't any packets 
             break;
         }
 
-        printf("packet header read. snap len: %d bytes\n", pcap_packet_header.caplen);
-
-        if (pcap_packet_header.caplen > pcap_header.snaplen) {
+        if (pcap_packet_header.incl_len > pcap_header.snaplen) {
             printf("Please enlarge packet buffer! We got packet with size: %d but our buffer is %d bytes\n",
-                pcap_packet_header.caplen, pcap_header.snaplen);
+                pcap_packet_header.incl_len, pcap_header.snaplen);
             return -4;
         }
 
-        ssize_t packet_payload_readed_bytes = read(filedesc, packet_bufer, pcap_packet_header.caplen);
+        ssize_t packet_payload_readed_bytes = read(filedesc, packet_bufer, pcap_packet_header.incl_len);
  
-        if (pcap_packet_header.caplen != packet_payload_readed_bytes) {
+        if (pcap_packet_header.incl_len != packet_payload_readed_bytes) {
             printf("I read packet header but can't read packet payload\n");
             return -3;
         }
 
-        printf("packet payload read\n");
+        // printf("packet payload read\n");
         
         read_packets++;
     }
