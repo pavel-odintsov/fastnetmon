39d08fc8f35fb6ec955b6ec3fed78ff3c2374c80
Pavel Odintsov <pavel.odintsov@gmail.com>
2015-03-20 11:52:16 +0300

    Enable compile on Mac OS X

--- tests/build_pcap_reader.sh
+++ tests/build_pcap_reader.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-clang++ ../netflow_plugin/netflow_collector.cpp -c -onetflow_collector.o
+clang++ ../netflow_plugin/netflow_collector.cpp -c -onetflow_collector.o -I/opt/local/include
 clang ../fastnetmon_packet_parser.cpp -c -ofastnetmon_packet_parser.o
-clang++ pcap_reader.cpp fastnetmon_packet_parser.o netflow_collector.o -o pcap_reader -llog4cpp
+clang++ pcap_reader.cpp fastnetmon_packet_parser.o netflow_collector.o -I/opt/local/include -L/opt/local/lib -o pcap_reader -llog4cpp
 
 rm -f fastnetmon_packet_parser.o
