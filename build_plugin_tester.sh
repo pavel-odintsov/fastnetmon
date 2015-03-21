873267dc4ef1825ab44759d0e806b29cc82f79ce
Pavel Odintsov <pavel.odintsov@gmail.com>
2015-03-21 14:41:08 +0300

    Remove copy & paste from plugin runner

--- build_plugin_tester.sh
+++ build_plugin_tester.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+g++ fast_library.cpp -c -ofast_library.o
 g++ netflow_plugin/netflow_collector.cpp -c -onetflow_collector.o
 g++ sflow_plugin/sflow_collector.cpp -c -osflow_collector.o
 g++ pcap_plugin/pcap_collector.cpp -c  -opcap_collector.o
 g++ pfring_plugin/pfring_collector.cpp -c  -opfring_collector.o -I/opt/pf_ring/include 
 
-g++ plugin_runner.cpp -lpcap -llog4cpp netflow_collector.o sflow_collector.o pcap_collector.o pfring_collector.o -oplugin_tester -I/opt/pf_ring/include -lpfring -lpthread -L/opt/pf_ring/lib
+g++ plugin_runner.cpp -lpcap -llog4cpp fast_library.o netflow_collector.o sflow_collector.o pcap_collector.o pfring_collector.o -oplugin_tester -I/opt/pf_ring/include -lpfring -lpthread -L/opt/pf_ring/lib -lboost_regex
 rm -f netflow_collector.o sflow_collector.o pcap_collector.o
 
--- plugin_runner.cpp
+++ plugin_runner.cpp
@@ -11,6 +11,7 @@
 #include <arpa/inet.h>
 
 #include "fastnetmon_types.h"
+#include "fast_library.h"
 #include "netflow_plugin/netflow_collector.h"
 #include "sflow_plugin/sflow_collector.h"
 #include "pcap_plugin/pcap_collector.h"
@@ -36,11 +37,6 @@ log4cpp::Category& logger = log4cpp::Category::getRoot();
 // Global map with parsed config file
 std::map<std::string, std::string> configuration_map;
 
-// convert string to integer
-int convert_string_to_integer(std::string line) {
-    return atoi(line.c_str());
-}
-
 void init_logging() {
     log4cpp::PatternLayout* layout = new log4cpp::PatternLayout(); 
     layout->setConversionPattern ("%d [%p] %m%n"); 
@@ -51,142 +47,6 @@ void init_logging() {
     logger.setPriority(log4cpp::Priority::INFO);
     logger.addAppender(appender);
     logger.info("Logger initialized!");
-}
-
-std::string get_printable_protocol_name(unsigned int protocol) {
-    std::string proto_name;
-
-    switch (protocol) {
-        case IPPROTO_TCP:
-            proto_name = "tcp";
-            break;
-        case IPPROTO_UDP:
-            proto_name = "udp";
-            break;
-        case IPPROTO_ICMP:
-            proto_name = "icmp";
-            break;
-        default:
-            proto_name = "unknown";
-            break;
-    } 
-
-    return proto_name;
-}
-
-string convert_timeval_to_date(struct timeval tv) {
-    time_t nowtime = tv.tv_sec;
-    struct tm *nowtm = localtime(&nowtime);
-    
-    char tmbuf[64];
-    char buf[64];
-
-    strftime(tmbuf, sizeof(tmbuf), "%Y-%m-%d %H:%M:%S", nowtm);
-
-    snprintf(buf, sizeof(buf), "%s.%06ld", tmbuf, tv.tv_usec); 
-
-    return string(buf);
-}
-
-string convert_ip_as_uint_to_string(uint32_t ip_as_integer) {
-    struct in_addr ip_addr;
-    ip_addr.s_addr = ip_as_integer;
-    return (string)inet_ntoa(ip_addr);
-}
-
-// http://stackoverflow.com/questions/14528233/bit-masking-in-c-how-to-get-first-bit-of-a-byte
-int extract_bit_value(uint8_t num, int bit) {
-    if (bit > 0 && bit <= 8) {
-        return ( (num >> (bit-1)) & 1 );
-    } else {
-        return 0;
-    }
-}
-
-string print_tcp_flags(uint8_t flag_value) {
-    if (flag_value == 0) {
-        return "-";
-    }
-
-    // cod from pfring.h
-    // (tcp->fin * TH_FIN_MULTIPLIER) + (tcp->syn * TH_SYN_MULTIPLIER) +
-    // (tcp->rst * TH_RST_MULTIPLIER) + (tcp->psh * TH_PUSH_MULTIPLIER) +
-    // (tcp->ack * TH_ACK_MULTIPLIER) + (tcp->urg * TH_URG_MULTIPLIER);
-
-    /*
-        // Required for decoding tcp flags
-        #define TH_FIN_MULTIPLIER   0x01
-        #define TH_SYN_MULTIPLIER   0x02
-        #define TH_RST_MULTIPLIER   0x04
-        #define TH_PUSH_MULTIPLIER  0x08
-        #define TH_ACK_MULTIPLIER   0x10
-        #define TH_URG_MULTIPLIER   0x20
-    */
-
-    vector<string> all_flags;
-
-    if (extract_bit_value(flag_value, 1)) {
-        all_flags.push_back("fin");
-    }
-    
-    if (extract_bit_value(flag_value, 2)) {
-        all_flags.push_back("syn");
-    }   
-
-    if (extract_bit_value(flag_value, 3)) {
-        all_flags.push_back("rst");
-    }   
-
-    if (extract_bit_value(flag_value, 4)) {
-        all_flags.push_back("psh");
-    }   
-
-    if (extract_bit_value(flag_value, 5)) {
-        all_flags.push_back("ack");
-    }    
-
-    if (extract_bit_value(flag_value, 6)) {
-        all_flags.push_back("urg");
-    }   
-
-    
-    ostringstream flags_as_string;
-
-    if (all_flags.empty()) {
-        return "-";
-    }
-
-    // concatenate all vector elements with comma
-    std::copy(all_flags.begin(), all_flags.end() - 1, std::ostream_iterator<string>(flags_as_string, ","));
-
-    // add last element
-    flags_as_string << all_flags.back();
-    
-    return flags_as_string.str();
-}
-
-string print_simple_packet(simple_packet packet) {
-    std::stringstream buffer;
-
-    buffer<<convert_timeval_to_date(packet.ts)<<" ";
-
-    buffer
-        <<convert_ip_as_uint_to_string(packet.src_ip)<<":"<<packet.source_port
-        <<" > "
-        <<convert_ip_as_uint_to_string(packet.dst_ip)<<":"<<packet.destination_port
-        <<" protocol: "<<get_printable_protocol_name(packet.protocol);
-   
-    // Print flags only for TCP 
-    if (packet.protocol == IPPROTO_TCP) { 
-        buffer<<" flags: "<<print_tcp_flags(packet.flags);
-    }
-
-    buffer<<" packets: "<<packet.number_of_packets<<" ";
-    buffer<<"size: "   <<packet.length<<" bytes"<<" ";
-    buffer<<"sample ratio: "<<packet.sample_ratio<<" ";
-    buffer<<"\n";
-    
-    return buffer.str();
 }
 
 void process_packet(simple_packet& current_packet) { 
