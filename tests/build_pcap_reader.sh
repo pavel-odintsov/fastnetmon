245d4dcad39ab41f4f3c0dfc58720bb89ed723b1
Pavel Odintsov <pavel.odintsov@gmail.com>
2015-03-21 14:32:27 +0300

    Remove copy & paste from pcap_reader. We use library functions now

--- tests/build_pcap_reader.sh
+++ tests/build_pcap_reader.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+clang++ ../fast_library.cpp -c -ofast_library.o
 clang++ ../netflow_plugin/netflow_collector.cpp -c -onetflow_collector.o -I/opt/local/include
 clang ../fastnetmon_packet_parser.cpp -c -ofastnetmon_packet_parser.o
-clang++ pcap_reader.cpp fastnetmon_packet_parser.o netflow_collector.o -I/opt/local/include -L/opt/local/lib -o pcap_reader -llog4cpp
+clang++ pcap_reader.cpp fastnetmon_packet_parser.o fast_library.o netflow_collector.o -I/opt/local/include -L/opt/local/lib -o pcap_reader -llog4cpp -lboost_system -lboost_regex
 
 rm -f fastnetmon_packet_parser.o
