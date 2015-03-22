7506704c0ce44c5f809f1ed7691cbcfa1b9c32a1
Pavel Odintsov <pavel.odintsov@gmail.com>
2015-03-23 00:22:56 +0300

    Add ipfix db support for plugin runner

--- src/build_plugin_tester.sh
+++ src/build_plugin_tester.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+g++ ipfix_rfc.cpp -c -oipfix_rfc.o
 g++ fast_library.cpp -c -ofast_library.o
 g++ netflow_plugin/netflow_collector.cpp -c -onetflow_collector.o
 g++ sflow_plugin/sflow_collector.cpp -c -osflow_collector.o
@@ -7,5 +8,5 @@ g++ pcap_plugin/pcap_collector.cpp -c  -opcap_collector.o
 g++ pfring_plugin/pfring_collector.cpp -c  -opfring_collector.o -I/opt/pf_ring/include 
 
 g++ plugin_runner.cpp -lpcap -llog4cpp fast_library.o netflow_collector.o sflow_collector.o pcap_collector.o pfring_collector.o -oplugin_tester -I/opt/pf_ring/include -lpfring -lpthread -L/opt/pf_ring/lib -lboost_regex
-rm -f netflow_collector.o sflow_collector.o pcap_collector.o
+rm -f netflow_collector.o ipfix_rfc.o sflow_collector.o pcap_collector.o
 
