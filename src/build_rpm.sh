f43d266ed77aae8c10e98332a9a709735122f388
Pavel Odintsov <pavel.odintsov@gmail.com>
2015-03-25 15:27:23 +0300

    Add spec file for Fedora 21

--- src/build_rpm.sh
+++ src/build_rpm.sh
@@ -14,13 +14,16 @@ mkdir vzapi-$VERSION
 mkdir -p /root/rpmbuild/SOURCES
 # Folder inside archive should be named as "fastnetmon-1.1.1"
 wget https://github.com/FastVPSEestiOu/fastnetmon/archive/master.zip -O"/root/rpmbuild/SOURCES/fastnetmon-$VERSION.tar.gz"
-#wget https://github.com/FastVPSEestiOu/fastnetmon/archive/v1.1.1.tar.gz -O"/root/rpmbuild/SOURCES/fastnetmon-$VERSION.tar.gz"
+
+# For Fedora package we use specific commit
+# wget https://github.com/FastVPSEestiOu/fastnetmon/archive/86b951b6dffae0fc1e6cbf66fe5f0f4aa61aaa5a/fastnetmon-86b951b6dffae0fc1e6cbf66fe5f0f4aa61aaa5a.tar.gz  -O"/root/rpmbuild/SOURCES/fastnetmon-86b951b6dffae0fc1e6cbf66fe5f0f4aa61aaa5a.tar.gz" 
 
 # Download spec file 
 wget https://raw.githubusercontent.com/FastVPSEestiOu/fastnetmon/master/src/fastnetmon_centos6.spec -Ofastnetmon_centos6.spec
 wget https://raw.githubusercontent.com/FastVPSEestiOu/fastnetmon/master/src/fastnetmon_centos7.spec -Ofastnetmon_centos7.spec
 
 # http://www.stableit.ru/2013/11/how-to-build-rpm-package-for-rhel5-on.html
-rpmbuild -bb fastnetmon.spec
+# Build binary and source package
+rpmbuild -ba fastnetmon.spec
 
 # For CentOS 7 we will use log4cpp from EPEL 7 testing repository: http://koji.fedoraproject.org/koji/buildinfo?buildID=623067
