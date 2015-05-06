02933b56a1e1cb701610493e0bba80eae65802fe
Pavel Odintsov <pavel.odintsov@gmail.com>
2015-05-06 23:55:26 +0300

    Fix compatibility with new work machine on Jessie

--- src/build_plugin_tester.sh
+++ src/build_plugin_tester.sh
@@ -7,6 +7,6 @@ g++ sflow_plugin/sflow_collector.cpp -c -osflow_collector.o
 g++ pcap_plugin/pcap_collector.cpp -c  -opcap_collector.o
 g++ pfring_plugin/pfring_collector.cpp -c  -opfring_collector.o -I/opt/pf_ring/include 
 
-g++ plugin_runner.cpp -lnuma -lpcap -llog4cpp ipfix_rfc.o fast_library.o netflow_collector.o sflow_collector.o pcap_collector.o pfring_collector.o -oplugin_tester -I/opt/pf_ring/include -lpfring -lpthread -L/opt/pf_ring/lib -lboost_regex
+g++ plugin_runner.cpp -lnuma -lpcap -llog4cpp ipfix_rfc.o fast_library.o netflow_collector.o sflow_collector.o pcap_collector.o pfring_collector.o -oplugin_tester -I/opt/pf_ring/include -lpfring -lpthread -L/opt/pf_ring/lib -lboost_regex -lboost_system
 rm -f netflow_collector.o ipfix_rfc.o sflow_collector.o pcap_collector.o
 
