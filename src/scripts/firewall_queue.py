88485bb886fbae91b567aec5277227d0c19f377b
Pavel Odintsov <pavel.odintsov@gmail.com>
2015-05-15 21:53:38 +0200

    Working code for Firewall Queue! Whoa! We could block traffic directly from ExaBGP hook

--- src/scripts/firewall_queue.py
+++ src/scripts/firewall_queue.py
@@ -1,8 +1,19 @@
 from subprocess import call
+import pprint
+# u'destination-ipv4': [u'10.0.0.2/32'],
+# u'destination-port': [u'=3128'],
+# u'protocol': [u'=tcp'],
+# u'source-ipv4': [u'10.0.0.1/32'],
+# u'string': u'flow destination-ipv4 10.0.0.2/32 source-ipv4 10.0.0.1/32 protocol =tcp destination-port =3128'}
 
-def execute_ip_ban(ip):
-    print "Will ban IP: " + ip + "\n"
-    call(["iptables", "-A", "INPUT", "-s", ip, "-j", "DROP"])
+def execute_ip_ban(flow):
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(flow)    
+
+    source_ip = flow["source-ipv4"][0]
+
+    print "Will ban IP: " + source_ip + "\n"
+    call(["iptables", "-A", "INPUT", "-s", source_ip, "-j", "DROP"])
     return True
 
 
