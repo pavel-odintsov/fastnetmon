56b8a1f97ac6b879ad119250259f38b5c23c1a32
Pavel Odintsov <pavel.odintsov@gmail.com>
2015-05-21 11:56:43 -0400

    Add ability to track bps/pps speed for each subnet; Add subnet length in cidr for get_direction(); Disable configuration dump on the client's screen; Related: #227

--- src/fast_library.h
+++ src/fast_library.h
@@ -77,5 +77,6 @@ bool read_pid_from_file(pid_t& pid, std::string pid_path);
 
 std::string convert_prefix_to_string_representation(prefix_t* prefix);
 std::string find_subnet_by_ip_in_string_format(patricia_tree_t* patricia_tree, std::string ip);
+std::string convert_subnet_to_string(subnet my_subnet);
 
 #endif
