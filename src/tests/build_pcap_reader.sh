508fe92a9be122f299723949effd2ddc3fd75112
Pavel Odintsov <pavel.odintsov@gmail.com>
2015-05-29 15:07:06 +0300

    Get rid of external bash script for building pcap. But still not worlking on Mac OS X

--- src/tests/build_pcap_reader.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-clang++ ../ipfix_rfc.cpp -c -oipfix_rfc.o
-clang++ ../fast_library.cpp -c -ofast_library.o  -I/opt/local/include
-clang++ ../netflow_plugin/netflow_collector.cpp -c -onetflow_collector.o -I/opt/local/include
-clang ../fastnetmon_packet_parser.c -c -ofastnetmon_packet_parser.o
-clang++ pcap_reader.cpp fastnetmon_packet_parser.o ipfix_rfc.o fast_library.o netflow_collector.o -I/opt/local/include -L/opt/local/lib -o pcap_reader -llog4cpp -lboost_system -lboost_regex
-
-rm -f fastnetmon_packet_parser.o
