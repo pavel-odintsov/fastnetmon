d72def1769a8af8990d96e326d996e07ba2c187d
Pavel Odintsov <pavel.odintsov@gmail.com>
2015-05-31 07:15:15 -0400

    Add test with pypy

--- src/tests/py_counters_performance_test.py
+++ src/tests/py_counters_performance_test.py
@@ -4,15 +4,17 @@ import time
 my_dict = {}
 
 
-# Intel(R) Core(TM) i7-3635QM CPU @ 2.40GHz: 4.8 MOPS
-# Intel(R) Core(TM) i7-3820 CPU @ 3.60GHz: 7.0 MOPS
+# Intel(R) Core(TM) i7-3635QM CPU @ 2.40GHz:                    4.8 MOPS
+# Intel(R) Core(TM) i7-3820 CPU @ 3.60GHz:   python 2.7         7.0 MOPS
+# Intel(R) Core(TM) i7-3820 CPU @ 3.60GHz:   python 2.7/pypy:   8.6 MOPS
 
 iterations = 10**6*14
 
 start = time.time()
+
 # Emulate 14.6 mpps
-for i in range(1, iterations):
-    my_dict[i] = i
+for index in range(0, iterations):
+    my_dict[index] = index
 
 stop = time.time()
 
