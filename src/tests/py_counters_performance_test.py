8dd102256b55a9c9ba5a4f95549650c8c9c018ee
Pavel Odintsov <pavel.odintsov@gmail.com>
2015-05-31 13:16:36 +0300

    Add script for Python's dict speed avaluation

---
+++ src/tests/py_counters_performance_test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import time
+my_dict = {}
+
+
+# Intel(R) Core(TM) i7-3635QM CPU @ 2.40GHz
+# 4.8 MOPS
+
+iterations = 10**6*14
+
+start = time.time()
+# Emulate 14.6 mpps
+for i in range(1, iterations):
+    my_dict[i] = i
+
+stop = time.time()
+
+print iterations / (stop - start) / 10**6, "millions of iterations per second"
+
