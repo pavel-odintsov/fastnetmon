83a22ca7094954a980984a671b920cfd41315257
Pavel Odintsov <pavel.odintsov@gmail.com>
2015-06-19 04:14:53 -0400

    Add comments about FreeBSD's promisc

--- src/tests/promisc.cpp
+++ src/tests/promisc.cpp
@@ -11,10 +11,18 @@
 
 // This code compiles on FreeBSD and Linux but did not work on FreeBSD 
 
+/* Promisc management on FreeBSD is real nighmare, really. Issues with: ifr_flagshigh and IFF_PPROMISC vs IFF_PROMISC */
+/* Good code examples here: https://github.com/fichtner/netmap/blob/master/extra/libpcap-netmap.diff */
+
 int main() {
     // We need really any socket for ioctl
 
     int fd = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    if (!fd) {
+        printf("Can't create socket\n");
+        exit(1);
+    }
 
     struct ifreq ethreq;    
     memset(&ethreq, 0, sizeof(ethreq));
