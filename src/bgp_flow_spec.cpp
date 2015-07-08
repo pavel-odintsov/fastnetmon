3e1618406875153d58ae8f74150c1e4c57c72b6e
Pavel Odintsov <pavel.odintsov@gmail.com>
2015-07-08 11:45:33 -0400

    Add BGP flow spec implementation stub

---
+++ src/bgp_flow_spec.cpp
@@ -0,0 +1,6 @@
+#include "bgp_flow_spec.h"
+
+void exabgp_flow_spec_rule_ban_manage(std::string action, flow_spec_rule_t flow_spec_rule) {
+// "announce flow route {\\n match {\\n source 10.0.0.1/32;\\nsource-port =" + str(i) +
+// ";\\n destination 1.2.3.4/32;\\n }\\n then {\\n discard;\\n }\\n }\\n\n")
+}
