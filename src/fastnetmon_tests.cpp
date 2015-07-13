ee77d0ee01e0cd227f14721d52a79708b1387a4c
Pavel Odintsov <pavel.odintsov@gmail.com>
2015-07-13 14:36:50 +0200

    Add complete integration tests and real call to ExaBGP for configurartion checks

--- src/fastnetmon_tests.cpp
+++ src/fastnetmon_tests.cpp
@@ -4,6 +4,8 @@
 #include "fast_library.h"
 #include "bgp_flow_spec.h"
 
+#include <fstream>
+
 #include "log4cpp/Category.hh"
 #include "log4cpp/Appender.hh"
 #include "log4cpp/FileAppender.hh"
@@ -218,6 +220,72 @@ TEST(BgpFlowSpec, serialize_then_first) {
 
     EXPECT_EQ( exabgp_rule.serialize_then(), "then {rate-limit 1024;}");
 }
+
+TEST(BgpFlowSpec, serialize_whole) {
+    bgp_flow_spec_action_t my_action;
+    //my_action.set_type(FLOW_SPEC_ACTION_ACCEPT);
+    my_action.set_type(FLOW_SPEC_ACTION_RATE_LIMIT);
+    my_action.set_rate_limit(1024); 
+
+    exabgp_flow_spec_rule_t exabgp_rule;
+    exabgp_rule.add_protocol(FLOW_SPEC_PROTOCOL_UDP);
+    exabgp_rule.add_source_port(53);
+    exabgp_rule.add_destination_port(80);
+
+    exabgp_rule.add_packet_length(777);
+    exabgp_rule.add_packet_length(1122);
+
+    exabgp_rule.add_fragmentation_flag(FLOW_SPEC_IS_A_FRAGMENT);
+    exabgp_rule.add_fragmentation_flag(FLOW_SPEC_DONT_FRAGMENT);
+
+    exabgp_rule.set_destination_subnet( convert_subnet_from_string_to_binary_with_cidr_format("127.0.0.0/24") );
+    exabgp_rule.set_source_subnet( convert_subnet_from_string_to_binary_with_cidr_format("4.0.0.0/24") );
+
+    exabgp_rule.set_action( my_action );
+    
+    exabgp_rule.disable_indents();
+
+    EXPECT_EQ( exabgp_rule.serialize(), "route {match {source 4.0.0.0/24;destination 127.0.0.0/24;protocol [ udp ];source-port [ =53 ];destination-port [ =80 ];packet-length [ =777 =1122 ];fragment [ is-fragment dont-fragment ];}then {rate-limit 1024;}}");
+}
+
+TEST(BgpFlowSpec, serialize_with_real_exabgp) {
+    bgp_flow_spec_action_t my_action;
+    //my_action.set_type(FLOW_SPEC_ACTION_ACCEPT);
+    my_action.set_type(FLOW_SPEC_ACTION_RATE_LIMIT);
+    my_action.set_rate_limit(1024); 
+
+    exabgp_flow_spec_rule_t exabgp_rule;
+    exabgp_rule.add_protocol(FLOW_SPEC_PROTOCOL_UDP);
+    exabgp_rule.add_source_port(53);
+    exabgp_rule.add_destination_port(80);
+
+    exabgp_rule.add_packet_length(777);
+    exabgp_rule.add_packet_length(1122);
+
+    exabgp_rule.add_fragmentation_flag(FLOW_SPEC_IS_A_FRAGMENT);
+    exabgp_rule.add_fragmentation_flag(FLOW_SPEC_DONT_FRAGMENT);
+
+    exabgp_rule.set_destination_subnet( convert_subnet_from_string_to_binary_with_cidr_format("127.0.0.0/24") );
+    exabgp_rule.set_source_subnet( convert_subnet_from_string_to_binary_with_cidr_format("4.0.0.0/24") );
+
+    exabgp_rule.set_action( my_action );
+        
+    //exabgp_rule.disable_indents();
+    std::string exabgp_configuration = exabgp_rule.serialize_complete_exabgp_configuration();
+
+    std::ofstream config_file;
+    config_file.open("/tmp/exabgp_test_config.conf", std::ios::trunc);
+
+    if (config_file.is_open()) {
+        config_file << exabgp_configuration;
+        config_file.close();
+    }
+
+    int system_ret_code = system("/usr/src/exabgp/sbin/exabgp --test /tmp/exabgp_test_config.conf 2>/dev/null");
+
+    EXPECT_EQ( system_ret_code, 0 );
+}
+
 
 // Flow Spec actions tests
 
