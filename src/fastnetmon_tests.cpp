7b096c06d6691c3d0ddbbc94c13e391d67e01e10
Pavel Odintsov <pavel.odintsov@gmail.com>
2015-07-13 10:05:26 +0200

    Have finished Google Test integration to the project. Closes #325

--- src/fastnetmon_tests.cpp
+++ src/fastnetmon_tests.cpp
@@ -4,6 +4,17 @@
 #include "fast_library.h"
 #include "bgp_flow_spec.h"
 
+#include "log4cpp/Category.hh"
+#include "log4cpp/Appender.hh"
+#include "log4cpp/FileAppender.hh"
+#include "log4cpp/OstreamAppender.hh"
+#include "log4cpp/Layout.hh"
+#include "log4cpp/BasicLayout.hh"
+#include "log4cpp/PatternLayout.hh"
+#include "log4cpp/Priority.hh"
+
+log4cpp::Category& logger = log4cpp::Category::getRoot();
+
 TEST(BgpFlowSpec, protocol_check_udp) {
     exabgp_flow_spec_rule_t exabgp_rule;
     exabgp_rule.add_protocol(FLOW_SPEC_PROTOCOL_UDP);
