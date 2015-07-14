29bced294d0eb371d57cc8b1fb80e886a4dc40cc
Pavel Odintsov <pavel.odintsov@gmail.com>
2015-07-14 18:10:35 +0200

    Switch to firehose application from SnabbSwitch from the custom code! Thanks to Luke!

--- src/tests/snabb/capturecallback.c
+++ src/tests/snabb/capturecallback.c
@@ -5,6 +5,54 @@
 #include <string.h>
 #include "../../fastnetmon_packet_parser.h"
 
+/* Called once before processing packets. */
+void firehose_start(); /* optional */
+
+/* Called once after processing packets. */
+void firehose_stop();  /* optional */
+
+/*
+ * Process a packet received from a NIC.
+ *
+ * pciaddr: name of PCI device packet is received from
+ * data:    packet payload (ethernet frame)
+ * length:  payload length in bytes
+ */
+inline void firehose_packet(const char *pciaddr, char *data, int length);
+
+/* Intel 82599 "Legacy" receive descriptor format.
+ * See Intel 82599 data sheet section 7.1.5.
+ * http://www.intel.com/content/dam/www/public/us/en/documents/datasheets/82599-10-gbe-controller-datasheet.pdf
+ */
+struct firehose_rdesc {
+  uint64_t address;
+  uint16_t length;
+  uint16_t cksum;
+  uint8_t status;
+  uint8_t errors;
+  uint16_t vlan;
+} __attribute__((packed));
+
+/* Traverse the hardware receive descriptor ring.
+ * Process each packet that is ready.
+ * Return the updated ring index.
+ */
+int firehose_callback_v1(const char *pciaddr,
+                         char **packets,
+                         struct firehose_rdesc *rxring,
+                         int ring_size,
+                         int index) {
+  while (rxring[index].status & 1) {
+    int next_index = (index + 1) & (ring_size-1);
+    __builtin_prefetch(packets[next_index]);
+    firehose_packet(pciaddr, packets[index], rxring[index].length);
+    rxring[index].status = 0; /* reset descriptor for reuse */
+    index = next_index;
+  }
+  return index;
+}
+
+
 uint64_t received_packets = 0;
 
 void* speed_printer(void* ptr) {
@@ -20,14 +68,15 @@ void* speed_printer(void* ptr) {
     }   
 }
 
-void run_speed_printer() {
+// We will start speed printer
+void firehose_start() {
     pthread_t thread;
     pthread_create(&thread, NULL, speed_printer, NULL);
 
     pthread_detach(thread);
 }
 
-void process_packet(char *data, int length) {
+void firehose_packet(const char *pciaddr, char *data, int length) {
     // Put packet to the cache
     struct pfring_pkthdr packet_header;
     memset(&packet_header, 0, sizeof(packet_header));
@@ -44,36 +93,5 @@ void process_packet(char *data, int length) {
 
     __sync_fetch_and_add(&received_packets, 1);
     //printf("Got packet with %d bytes.\n", length);
-}
-
-//
-// DMA processing callback for Lua
-//
-
-// Legacy receive descriptor format.
-// See 82599 data sheet section 7.1.5.
-struct rdesc {
-  uint64_t address;
-  uint16_t length;
-  uint16_t cksum;
-  uint8_t status;
-  uint8_t errors;
-  uint16_t vlan;
-} __attribute__((packed));
-
-// Traverse the hardware receive descriptor ring.
-// Process each packet that is ready.
-// Return the updated ring indx.
-int process_packets(char **packets,       // array of packet data buffers
-                    struct rdesc *rxring, // hardware RX descriptor ring
-                    int ring_size,        // size of ring
-                    int index,
-                    int max) {          // current index into ring
-  while (max-- > 0 && (rxring[index].status & 1)) { // packet ready?
-    process_packet(packets[index], rxring[index].length); // process packet
-    rxring[index].status = 0;                     // reset descriptor
-    index = (index + 1) & (ring_size-1);          // move on to next ring item
-  }
-  return index;
 }
 
