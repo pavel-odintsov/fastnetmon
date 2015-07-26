fe02d64d740669edf58738996ecc2607dbeb0cba
Pavel Odintsov <pavel.odintsov@gmail.com>
2015-07-26 14:45:40 +0300

    Remove old not working code from the reposiroty

--- syn_umbrella/build_syn_umbrella.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-g++ syn_umbrella.cpp -I/opt/crafter/include -I/opt/pf_ring/include -lpthread -L/opt/pf_ring/lib/ -lpfring -lnuma -L/opt/crafter/lib -lcrafter -o syn_umbrella
-
-
-# ./umbrella -i zc:eth4 -c 1 -o zc:eth4 -g 0 -v 
