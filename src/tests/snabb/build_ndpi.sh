33fc7d1be12dd2c90ca6d9aca75258a0959b90a7
Pavel Odintsov <pavel.odintsov@gmail.com>
2015-08-03 17:47:01 +0300

    Add GC for nDPI tracker; Add TSC timers;  Add build script.

---
+++ src/tests/snabb/build_ndpi.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+g++ -O3  ../../fastnetmon_packet_parser.c  -c -o fastnetmon_packet_parser.o -fPIC
+g++ -O3 ../../fastnetmon_pcap_format.cpp -c -o  fastnetmon_pcap_format.o -fPIC 
+g++ -O3  ../../fast_dpi.cpp -c -o  fast_dpi.o `PKG_CONFIG_PATH=/opt/ndpi/lib/pkgconfig pkg-config  pkg-config --cflags --libs libndpi` -fPIC
+g++ -O3  -shared -o ndpicallback.so -fPIC ndpicallback.cpp fastnetmon_pcap_format.o fast_dpi.o fastnetmon_packet_parser.o `PKG_CONFIG_PATH=/opt/ndpi/lib/pkgconfig pkg-config  pkg-config --cflags --libs libndpi` -std=c++11 -fPIC
