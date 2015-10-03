f5bd1486e5b5b35c3de123c2ac4b271d9ac5b37e
Pavel Odintsov <pavel.odintsov@gmail.com>
2015-10-03 21:15:56 +0200

    Add gobgp action module stub

---
+++ src/actions/gobgp_action.h
@@ -0,0 +1,9 @@
+#ifndef GOBGP_ACTION_H
+#define GOBGP_ACTION_H
+
+#include <string>
+#include "../fastnetmon_types.h"
+
+void gobgp_ban_manage(std::string action, std::string ip_as_string, attack_details current_attack);
+
+#endif
