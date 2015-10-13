e4571d0d64d0e3281e0e2d9c38e9449c4b5fdf48
Pavel Odintsov <pavel.odintsov@gmail.com>
2015-10-13 18:45:27 +0200

    Add API implementation examples in gRPC

---
+++ src/api_server.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include <grpc++/grpc++.h>
+
+#include "fastnetmon.grpc.pb.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+using fastmitigation::BanListRequest;
+using fastmitigation::BanListReply;
+using fastmitigation::Fastnetmon;
+
+// Logic and data behind the server's behavior.
+class GreeterServiceImpl final : public Fastnetmon::Service {
+    Status GetBanlist(ServerContext* context, const BanListRequest* request, BanListReply* reply) override {
+        std::string prefix("Hello ");
+        reply->set_message(prefix + request->name());
+        return Status::OK;
+    }
+};
+
+void RunServer() {
+    std::string server_address("0.0.0.0:50051");
+    GreeterServiceImpl service;
+
+    ServerBuilder builder;
+    // Listen on the given address without any authentication mechanism.
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    // Register "service" as the instance through which we'll communicate with
+    // clients. In this case it corresponds to an *synchronous* service.
+    builder.RegisterService(&service);
+    // Finally assemble the server.
+    std::unique_ptr<Server> server(builder.BuildAndStart());
+    std::cout << "Server listening on " << server_address << std::endl;
+
+    // Wait for the server to shutdown. Note that some other thread must be
+    // responsible for shutting down the server for this call to ever return.
+    server->Wait();
+}
+
+int main(int argc, char** argv) {
+    RunServer();
+
+    return 0;
+}
