70b6b4df3d767ff58d04c9a813b5cd149d487333
Pavel Odintsov <pavel.odintsov@gmail.com>
2016-01-08 18:21:59 +0300

    Make installer code more verbose for simpler debug

--- src/fastnetmon_install.pl
+++ src/fastnetmon_install.pl
@@ -1472,8 +1472,8 @@ sub install_fastnetmon {
         system("cmake .. $cmake_params");
         system("make $make_options");
     } else {
-        exec_command("cmake .. $cmake_params");
-        exec_command("make $make_options");
+        system("cmake .. $cmake_params");
+        system("make $make_options");
     }
 
     my $fastnetmon_dir = "/opt/fastnetmon";
