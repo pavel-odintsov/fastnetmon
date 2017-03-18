fdd37b314077a55e37b7cbde1136b479f4a1f038
Eric Chou <echou@yahoo.com>
2017-03-18 17:39:23 +0000

    a10.py modification for PEP476, updated REDME.md

--- src/a10_plugin/a10.py
+++ src/a10_plugin/a10.py
@@ -1,10 +1,10 @@
 
 #
-# v0.1 
+# v0.2
 # ericc@a10networks.com
 #
 
-import json, urllib2
+import json, urllib2, ssl
 
 def axapi_auth(host, username, password):
     base_uri = 'https://'+host
@@ -15,26 +15,28 @@ def axapi_auth(host, username, password):
 
 
 def axapi_action(uri, payload='', signature='', method='POST'):
+    # PEP476  2.7.9+ / 3.4.3+ cert check
+    new_context = ssl._create_unverified_context()
     try:
         if method == 'POST':
             req = urllib2.Request(uri)
             req.add_header('content-type', 'application/json')
             if signature:
                 req.add_header('Authorization', 'A10 {0}'.format(signature))
-            response = urllib2.urlopen(req, json.dumps(payload))
+            response = urllib2.urlopen(req, json.dumps(payload), context=new_context)
         elif method == 'GET':
             req = urllib2.Request(uri)
             req.add_header('content-type', 'application/json')
             if signature:
                 req.add_header('Authorization', 'A10 {0}'.format(signature))
-            response = urllib2.urlopen(req)
+            response = urllib2.urlopen(req, context=new_context)
         elif method == 'DELETE':
             req = urllib2.Request(uri)
             req.add_header('content-type', 'application/json')
             req.get_method = lambda: 'DELETE'
             if signature:
                 req.add_header('Authorization', 'A10 {0}'.format(signature))
-            response = urllib2.urlopen(req)
+            response = urllib2.urlopen(req, context=new_context)
         return response.read()
     except Exception as e:
         raise 
