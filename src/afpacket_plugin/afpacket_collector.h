42e85db9d974e238f12bad3796c42a64c0a239c2
Pavel Odintsov <pavel.odintsov@gmail.com>
2020-01-27 20:56:45 +0000

    Extracted AF_PACKET logic into function

--- src/afpacket_plugin/afpacket_collector.h
+++ src/afpacket_plugin/afpacket_collector.h
@@ -4,5 +4,6 @@
 #include "../fastnetmon_types.h"
 
 void start_afpacket_collection(process_packet_pointer func_ptr);
+void start_af_packet_capture_for_interface(std::string capture_interface, int fanout_group_id, unsigned int num_cpus);
 
 #endif
