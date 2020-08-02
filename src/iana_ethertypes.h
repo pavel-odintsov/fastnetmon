5285bc36efa8110de47db2d05cc39b32ce6e4cac
Pavel Odintsov <pavel.odintsov@gmail.com>
2020-08-02 12:04:37 +0100

    Added new structures for network packets processing #833 (#834)

---
+++ src/iana_ethertypes.h
@@ -0,0 +1,12 @@
+#pragma once
+enum IanaEthertype : unsigned int {
+
+    IanaEthertypeIPv4            = 2048,
+    IanaEthertypeARP             = 2054,
+    IanaEthertypeVLAN            = 33024,
+    IanaEthertypeIPv6            = 34525,
+    IanaEthertypeMPLS_unicast    = 34887,
+    IanaEthertypeMPLS_multicast  = 34888,
+    IanaEthertypePPPoE_discovery = 34915,
+    IanaEthertypePPPoE_session   = 34916,
+};
