ad5533bee2f690f76179347a6d9f6902fc58acc5
Pavel Odintsov <pavel.odintsov@gmail.com>
2020-08-23 14:29:12 +0100

    Extracted endianless conversion functions to separate file

--- src/fast_library.cpp
+++ src/fast_library.cpp
@@ -37,35 +37,6 @@ int convert_string_to_integer(std::string line) {
     return atoi(line.c_str());
 }
 
-// Type safe versions of ntohl, ntohs with type control
-uint16_t fast_ntoh(uint16_t value) {
-    return ntohs(value);
-}
-
-uint32_t fast_ntoh(uint32_t value) {
-    return ntohl(value);
-}
-
-// network (big endian) byte order to host byte order
-uint64_t fast_ntoh(uint64_t value) {
-    return be64toh(value);
-}
-
-// Type safe version of htonl, htons
-uint16_t fast_hton(uint16_t value) {
-    return htons(value);
-}
-
-uint32_t fast_hton(uint32_t value) {
-    return htonl(value);
-}
-
-uint64_t fast_hton(uint64_t value) {
-    // host to big endian (network byte order)
-    return htobe64(value);
-}
-
-
 uint32_t convert_ip_as_string_to_uint(std::string ip) {
     struct in_addr ip_addr;
     inet_aton(ip.c_str(), &ip_addr);
