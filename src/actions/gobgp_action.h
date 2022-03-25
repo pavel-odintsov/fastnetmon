13854accaad85d7fea854d8b9b2e3cf51d4b9732
Pavel Odintsov <pavel.odintsov@gmail.com>
2022-03-25 19:34:29 +0000

    We've started calling GoBGP callback for IPv6 traffic

--- src/actions/gobgp_action.h
+++ src/actions/gobgp_action.h
@@ -6,6 +6,6 @@
 
 void gobgp_action_init();
 void gobgp_action_shutdown();
-void gobgp_ban_manage(std::string action, std::string ip_as_string, attack_details_t current_attack);
+void gobgp_ban_manage(std::string action, bool ipv6, std::string ip_as_string, subnet_ipv6_cidr_mask_t client_ipv6, attack_details_t current_attack);
 
 #endif
