0babd49edcc025a2696e6fcfa75585bfa56e3cc4
Pavel Odintsov <pavel.odintsov@gmail.com>
2022-04-20 21:31:13 +0100

    Updated paths and improved commends for reformat script

--- src/scripts/reformat_code_with_clang_format.sh
+++ src/scripts/reformat_code_with_clang_format.sh
@@ -1,7 +1,10 @@
 #/usr/bin/bash
 
+# If you want to prevent some code from automatic reformatting, please add it to .clang_formatter_excludes file
+
 echo "We will reformat all your code"
-for file in `find /home/odintsov/repos/fastnetmon_github/src -type f | egrep '\.(c|cpp|hpp|h)$' | egrep -vf /home/odintsov/repos/fastnetmon_github/src/.clang_formatter_excludes`;
+
+for file in `find /home/odintsov/repos/fastnetmon_community/src -type f | egrep '\.(c|cpp|hpp|h)$' | egrep -vf /home/odintsov/repos/fastnetmon_community/src/.clang_formatter_excludes`;
 do
     echo "Reformattting $file";
     clang-format -style=file -i $file;
