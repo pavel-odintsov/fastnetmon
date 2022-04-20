5bf5beece17f46c48d02e145732856244c5b1b40
Pavel Odintsov <pavel.odintsov@gmail.com>
2022-04-20 22:04:17 +0100

    Added missing algorithm include

--- src/abstract_subnet_counters.hpp
+++ src/abstract_subnet_counters.hpp
@@ -2,6 +2,7 @@
 
 #include <mutex>
 #include <unordered_map>
+#include <algorithm>
 
 // I keep these declaration here because of following error:
 // error: there are no arguments to ‘increment_outgoing_counters’ that depend on a template parameter, so a declaration
