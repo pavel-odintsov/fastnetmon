c2dac3564ceaa1bef1b5bfac4d5bc544392ec1e3
Pavel Odintsov <pavel.odintsov@gmail.com>
2022-05-20 15:30:13 +0100

    Removed old unused code

--- src/tests/build_netmap.bash
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-clang++ fastnetmon_packet_parser.cpp -c -ofastnetmon_packet_parser.o
-clang++ netmap.cpp -I/usr/local/include -L/usr/local/lib -I/usr/src/fastnetmon/tests/netmap_includes -lboost_thread -lboost_system fastnetmon_packet_parser.o
-
