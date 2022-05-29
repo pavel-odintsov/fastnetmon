6d19e1ede489da68c69b5d1769d37498d828520c
Pavel Odintsov <pavel.odintsov@gmail.com>
2022-05-29 15:05:08 +0100

    Removed old unused scripts from code tree

--- src/scripts/README.md
+++
@@ -1,1 +0,0 @@
-We moved documentation to our [site](https://fastnetmon.com/subnet-collection-from-bgp-peering-session/)
--- src/scripts/bgp_network_retriever.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-import shelve
-database = shelve.open("/var/lib/bgp_network_collector.db")
-for key in sorted(database):
-    print key
