d005cba46c145e55668479b4f484425ce4072121
Pavel Odintsov <pavel.odintsov@gmail.com>
2022-06-03 09:53:11 +0100

    Unified library bundle build logic between all editions

--- src/scripts/build_libary_bundle.pl
+++ src/scripts/build_libary_bundle.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/perl
 
+
+
 use strict;
 use warnings;
 
@@ -9,7 +11,7 @@ use File::Basename;
 use File::Path qw(make_path);
 
 #
-# This script will produce binary archive with all libraries which required for FastNetMon
+# This script will produce binary archive with all libraries required for FastNetMon
 # 
 # Script params: path to bundle archive
 #
@@ -25,29 +27,37 @@ if (-e $archive_bundle_name) {
     unlink $archive_bundle_name;
 }
 
-my $global_path = '/opt/fastnetmon-community';
+# It may be different for community edition
+my $project_folder_name = 'fastnetmon-community';
+
+my $global_path = "/opt/$project_folder_name";
 my $fastnetmon_libraries_path = "$global_path/libraries";
 
-my $target_path = `mktemp -d`;
-chomp $target_path;
+my $temp_build_folder_path = `mktemp -d`;
+chomp $temp_build_folder_path;
 
-unless (-e $target_path && -d $target_path) {
-    die "Can't create target path\n";
+print "We will create temp folder for build: $temp_build_folder_path\n";
+
+unless (-e $temp_build_folder_path && -d $temp_build_folder_path) {
+    die "Can't create temporary folder for build\n";
 }
 
-print "We will create temp folder: $target_path\n";
+# Path to FastNetMon's folder int temp folder
+my $temp_folder_global_path = "$temp_build_folder_path/$project_folder_name";
 
+# Open path which has all developer libraries (including binaries)
 opendir my $fastnetmon_libs_handle, $fastnetmon_libraries_path or die "Could not open library directory $fastnetmon_libraries_path with error: $!";
 
 # List all files and folders in directory excluding special files . and ..
 my @libraries_list = grep { !/^\.+$/ } readdir $fastnetmon_libs_handle;
 closedir $fastnetmon_libs_handle; 
 
+# Iterate all libraries in developer folder
 for my $library (@libraries_list) {
     my $library_path = "$fastnetmon_libraries_path/$library";
 
     unless (-e $library_path) {
-        die "Can't find library $library please check\n";
+        die "Can't find library $library at path $library_path please check\n";
     }
 
     # We do not need these libraries on customer machines
@@ -74,11 +84,11 @@ for my $library (@libraries_list) {
             # print "$dir_name $file_name\n";
 
             my $target_full_path = $file_full_path;
-            $target_full_path =~ s#^$global_path#$target_path/fastnetmon#;
+            $target_full_path =~ s#^$global_path#$temp_folder_global_path#;
 
             # Create target folder 
             my $target_full_folder_path = $dir_name;
-            $target_full_folder_path =~ s#^$global_path#$target_path/fastnetmon/#;
+            $target_full_folder_path =~ s#^$global_path#$temp_folder_global_path/#;
 
             unless (-e $target_full_folder_path) {
                 # print "Create folder $target_full_folder_path\n";
@@ -89,57 +99,68 @@ for my $library (@libraries_list) {
                 my $symlink_target_name = readlink($file_full_path);
 
                 #print "We have symlink which aims to $symlink_target_name\n";
-
+ 
                 # This way we copy symlinks
                 my $symlink_result = symlink($symlink_target_name, $target_full_path);
 
                 unless ($symlink_result) {
                     die "Symlink from $symlink_target_name to $target_full_path failed\n";
-                }
+                } 
             } else {
                 copy($file_full_path, $target_full_folder_path);
 
                 # Strip debug information from library, we need it to reduce distribution size
-                # It's pretty serious disk space saving (from 260Mb to 95Mb in my tests)
-                system("strip --strip-debug $target_full_path");
+		        # It's pretty serious disk space saving (from 260Mb to 95Mb in my tests)
+		        system("strip --strip-debug $target_full_path");
             }
         }
     }
 }
 
+sub list_files_in_folder {
+    my $folder_path = shift;
+        
+    opendir my $fastnetmon_libs_handle, $folder_path or die "Could not open directory: $folder_path";
+    my @libraries_list = grep { !/^\.+$/ } readdir $fastnetmon_libs_handle;
+    closedir $fastnetmon_libs_handle;
 
-# Manually handle toolkit itself
-mkdir "$target_path/fastnetmon";
-copy("$global_path/fastnetmon/fastnetmon",        "$target_path/fastnetmon/fastnetmon");
-copy("$global_path/fastnetmon/fastnetmon_client", "$target_path/fastnetmon/fastnetmon_client");
-copy("$global_path/fastnetmon/fastnetmon_api_client", "$target_path/fastnetmon/fastnetmon_api_client");
+    return [ @libraries_list ];
+}
+
+### Copy binary files
+
+mkdir "$temp_folder_global_path";
+copy("$global_path/fastnetmon",             "$temp_folder_global_path/fastnetmon");
+copy("$global_path/fastnetmon_client",      "$temp_folder_global_path/fastnetmon_client");
+copy("$global_path/fastnetmon_api_client",  "$temp_folder_global_path/fastnetmon_api_client");
 
 # Set exec flag
-chmod 0755, "$target_path/fastnetmon/fastnetmon";
-chmod 0755, "$target_path/fastnetmon/fastnetmon_client";
-chmod 0755, "$target_path/fastnetmon/fastnetmon_api_client";
+chmod 0755, "$temp_folder_global_path/fastnetmon";
+chmod 0755, "$temp_folder_global_path/fastnetmon_client";
+chmod 0755, "$temp_folder_global_path/fastnetmon_api_client";
 
-# Install GoBGP's binary files
+# TODO: upgrade it to 2.27
+# Install GoBGP binary files
 my $gobgp_folder_name = "gobgp_2_17_0";
-mkdir "$target_path/fastnetmon-community/libraries/$gobgp_folder_name";
+mkdir "$temp_folder_global_path/libraries/$gobgp_folder_name";
 
 for my $gobgp_binary ('gobgp', 'gobgpd') {
     unless (-e "$fastnetmon_libraries_path/$gobgp_folder_name/$gobgp_binary") {
         die "GoBGP binary $gobgp_binary does not exist\n";
-    }   
+    }
 
     my $gobgp_copy_result = copy("$fastnetmon_libraries_path/$gobgp_folder_name/$gobgp_binary",
-        "$target_path/fastnetmon-community/libraries/$gobgp_folder_name/$gobgp_binary");
+        "$temp_folder_global_path/libraries/$gobgp_folder_name/$gobgp_binary");
 
     unless ($gobgp_copy_result) {
         die "Could not copy GoBGP's binary $gobgp_binary $!\n";
-    }   
+    }
 
     # Enable exec flag
-    chmod 0755, "$target_path/fastnetmon-community/libraries/$gobgp_folder_name/$gobgp_binary";
+    chmod 0755, "$temp_folder_global_path/libraries/$gobgp_folder_name/$gobgp_binary";
 }
 
 # Just tar it. We do not need compression here because this bundle will be unpacked by our own code later
 # I explicitly set uid and git to zero here to prevent any non zero owners for production build
-`tar -cpf $archive_bundle_name -C $target_path --owner=0 --group=0 ./`;
+`tar -cpf $archive_bundle_name -C $temp_build_folder_path --owner=0 --group=0 ./`;
 print "We have created bundle $archive_bundle_name\n";
