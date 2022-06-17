da442d101e144e10d14bcf24a0ab56e9c741f17c
Pavel Odintsov <pavel.odintsov@gmail.com>
2022-06-17 15:15:30 +0100

    Switched Mac build to new version

--- src/packaging/homebrew/fastnetmon.rb
+++ src/packaging/homebrew/fastnetmon.rb
@@ -1,9 +1,9 @@
 class Fastnetmon < Formula
   desc "DDoS detection tool with sFlow, Netflow, IPFIX and port mirror support"
   homepage "https://github.com/pavel-odintsov/fastnetmon/"
-  url "https://github.com/pavel-odintsov/fastnetmon/archive/5d6a96ca4c2bef05d65f1d0a6df3ba0673ced171.tar.gz"
+  url "https://github.com/pavel-odintsov/fastnetmon/archive/1da7b6ee13586ed75ed496c193a176654c7dff13.tar.gz"
   version "1.2.2"
-  sha256 "0b307662158cf3cd8f55b3ea097d93995de81142ed64e5ad8aabe7dce8d096ea"
+  sha256 "552faba1981896788281d40c7d0e6462743cfdc658431f0a99d90478407b11e3"
   license "GPL-2.0-only"
   revision 1
 
