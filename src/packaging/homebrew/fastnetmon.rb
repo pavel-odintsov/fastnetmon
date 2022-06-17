44c3e192d5cf916fcdac4939974bf32788b8821f
Pavel Odintsov <pavel.odintsov@gmail.com>
2022-06-17 14:46:22 +0100

    Switched to latest commit for HomeBrew

--- src/packaging/homebrew/fastnetmon.rb
+++ src/packaging/homebrew/fastnetmon.rb
@@ -1,9 +1,9 @@
 class Fastnetmon < Formula
   desc "DDoS detection tool with sFlow, Netflow, IPFIX and port mirror support"
   homepage "https://github.com/pavel-odintsov/fastnetmon/"
-  url "https://github.com/pavel-odintsov/fastnetmon/archive/9ec38c73ee4a4ff7af1f29d07af2a1e58e513af0.tar.gz"
+  url "https://github.com/pavel-odintsov/fastnetmon/archive/5d6a96ca4c2bef05d65f1d0a6df3ba0673ced171.tar.gz"
   version "1.2.2"
-  sha256 "2451c4b65579a8e991dcb88a1e2ad6556ab01b23a79867d871b5e0148530b419"
+  sha256 "0b307662158cf3cd8f55b3ea097d93995de81142ed64e5ad8aabe7dce8d096ea"
   license "GPL-2.0-only"
   revision 1
 
