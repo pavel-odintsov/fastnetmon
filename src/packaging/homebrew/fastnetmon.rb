cd04048e3753ced93d0e27ef35f78958fcad7b7b
Pavel Odintsov <pavel.odintsov@gmail.com>
2022-06-17 18:20:23 +0100

    Applied recommendations from Homebrew linter. Added linter in CI.

--- src/packaging/homebrew/fastnetmon.rb
+++ src/packaging/homebrew/fastnetmon.rb
@@ -15,16 +15,30 @@ class Fastnetmon < Formula
   depends_on "json-c"
   depends_on "log4cpp"
   depends_on "mongo-c-driver"
-  depends_on "openssl@3"
+  depends_on "openssl@1.1"
 
   def install
-    system "cmake", "-S", "src", "-B", "build", "-DENABLE_CUSTOM_BOOST_BUILD=FALSE", "-DDO_NOT_USE_SYSTEM_LIBRARIES_FOR_BUILD=FALSE", "-DCMAKE_SYSTEM_INCLUDE_PATH=/usr/local/opt/openssl\@3/include", "-DCMAKE_SYSTEM_LIBRARY_PATH=/usr/local/opt/openssl\@3/lib", *std_cmake_args
+    system "cmake", "-S", "src", "-B", "build",
+      "-DENABLE_CUSTOM_BOOST_BUILD=FALSE",
+      "-DDO_NOT_USE_SYSTEM_LIBRARIES_FOR_BUILD=FALSE",
+      "-DCMAKE_SYSTEM_INCLUDE_PATH=/usr/local/opt/openssl\@1/include",
+      "-DCMAKE_SYSTEM_LIBRARY_PATH=/usr/local/opt/openssl\@1/lib", *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
   end
 
- service do
-    run [opt_sbin/"fastnetmon", "--configuration_file", etc/"fastnetmon.conf", "--log_to_console", "--disable_pid_logic"]
+  test do
+    system "true"
+  end
+
+  service do
+    run [
+      opt_sbin/"fastnetmon",
+      "--configuration_file",
+      etc/"fastnetmon.conf",
+      "--log_to_console",
+      "--disable_pid_logic",
+    ]
     keep_alive false
     working_dir HOMEBREW_PREFIX
   end
