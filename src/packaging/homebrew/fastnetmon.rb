4c90bfa0c9462303f1916e6d54a38c4a64e10058
Pavel Odintsov <pavel.odintsov@gmail.com>
2022-06-22 19:31:16 +0100

    Sync with HomeBrew upstream: https://formulae.brew.sh/formula/fastnetmon

--- src/packaging/homebrew/fastnetmon.rb
+++ src/packaging/homebrew/fastnetmon.rb
@@ -1,9 +1,7 @@
 class Fastnetmon < Formula
   desc "DDoS detection tool with sFlow, Netflow, IPFIX and port mirror support"
   homepage "https://github.com/pavel-odintsov/fastnetmon/"
-  url "https://github.com/pavel-odintsov/fastnetmon/archive/78baf3be83bb56e03528a96fe6df37eb20876cd9.tar.gz"
-  version "1.2.2"
-  sha256 "ed9376fc193cbd2dd36eb5e80a2d03143f2ab0b6b4d9d74ae3572cc544754b20"
+  head "https://github.com/pavel-odintsov/fastnetmon.git"
   license "GPL-2.0-only"
 
   depends_on "cmake" => :build
@@ -17,6 +15,7 @@ class Fastnetmon < Formula
   depends_on macos: :big_sur # We need C++ 20 available for build which is available from Big Sur
   depends_on "mongo-c-driver"
   depends_on "openssl@1.1"
+  uses_from_macos "ncurses"
 
   on_linux do
     depends_on "gcc"
@@ -28,12 +27,11 @@ class Fastnetmon < Formula
 
   def install
     system "cmake", "-S", "src", "-B", "build",
-      "-DENABLE_CUSTOM_BOOST_BUILD=FALSE",
-      "-DDO_NOT_USE_SYSTEM_LIBRARIES_FOR_BUILD=FALSE",
-      "-DLINK_WITH_ABSL=TRUE",
-      "-DSET_ABSOLUTE_INSTALL_PATH=OFF",
-      "-DCMAKE_SYSTEM_INCLUDE_PATH=#{Formula["openssl@1.1"].include}",
-      "-DCMAKE_SYSTEM_LIBRARY_PATH=#{Formula["openssl@1.1"].lib}", *std_cmake_args
+                    "-DENABLE_CUSTOM_BOOST_BUILD=FALSE",
+                    "-DDO_NOT_USE_SYSTEM_LIBRARIES_FOR_BUILD=FALSE",
+                    "-DLINK_WITH_ABSL=TRUE",
+                    "-DSET_ABSOLUTE_INSTALL_PATH=OFF",
+                    *std_cmake_args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
   end
@@ -61,22 +59,22 @@ class Fastnetmon < Formula
 
     fastnetmon_pid = fork do
       exec opt_sbin/"fastnetmon",
-      "--configuration_file",
-      testpath/"fastnetmon.conf",
-      "--log_to_console",
-      "--disable_pid_logic"
+           "--configuration_file",
+           testpath/"fastnetmon.conf",
+           "--log_to_console",
+           "--disable_pid_logic"
     end
 
     sleep 15
 
     assert_path_exists testpath/"fastnetmon.dat"
 
-    ipv4_stats_output = File.read(testpath/"fastnetmon.dat")
+    ipv4_stats_output = (testpath/"fastnetmon.dat").read
     assert_match("Incoming traffic", ipv4_stats_output)
 
     assert_path_exists testpath/"fastnetmon_ipv6.dat"
 
-    ipv6_stats_output = File.read(testpath/"fastnetmon_ipv6.dat")
+    ipv6_stats_output = (testpath/"fastnetmon_ipv6.dat").read
     assert_match("Incoming traffic", ipv6_stats_output)
   ensure
     Process.kill "SIGTERM", fastnetmon_pid
