485d52347d6a6e3b6768d2c73b4a1c5b47f36e5c
Pavel Odintsov <pavel.odintsov@gmail.com>
2022-07-01 15:32:36 +0100

    Added full scan test

--- src/tests/traffic_structures_performance_tests_real_traffic.cpp
+++ src/tests/traffic_structures_performance_tests_real_traffic.cpp
@@ -72,14 +72,49 @@ void run_tests(std::vector<uint32_t> our_ips, T& data_structure) {
 
     float megaops_per_second = (float)total_ops / ((float)total_used_nanoseconds / (float)1000000000) / 1000000;
 
-    std::cout << "Total time is " << used_seconds << " seconds total ops: " << total_ops << std::endl;
-    std::cout << "Million of ops per second: " << megaops_per_second << std::endl;
+    std::cout << "Total lookup time is " << used_seconds << " seconds" << std::endl;
+    std::cout << "Million of lookup ops per second: " << megaops_per_second << std::endl;
 
 #ifdef __MACH__
     mach_port_deallocate(mach_task_self(), cclock);
 #endif
 }
 
+// Runs tests for specific structure
+template <typename T>
+void run_scan_tests(T& data_structure, uint64_t& accumulator) {
+    struct timespec start_time;
+    clock_gettime(CLOCK_REALTIME, &start_time);
+
+    unsigned long total_ops = 0;
+
+    uint64_t number_of_reruns = 1000;
+
+    total_ops = number_of_reruns * data_structure.size();
+
+    for (int j = 0; j < number_of_reruns; j++) {
+        for (const auto& elem: data_structure) {
+            accumulator += elem.second.udp.in_bytes;
+        }
+    }
+
+    struct timespec finish_time;
+    clock_gettime(CLOCK_REALTIME, &finish_time);
+
+    unsigned long used_seconds     = finish_time.tv_sec - start_time.tv_sec;
+    unsigned long used_nanoseconds = finish_time.tv_nsec - start_time.tv_nsec;
+
+    unsigned long total_used_nanoseconds = used_seconds * 1000000000 + used_nanoseconds;
+
+    float megaops_per_second = (float)total_ops / ((float)total_used_nanoseconds / (float)1000000000) / 1000000;
+
+    std::cout << "Total scan time is " << used_seconds << " seconds" << std::endl;
+    std::cout << "Million of full scan ops per second: " << megaops_per_second << std::endl;
+
+#ifdef __MACH__
+    mach_port_deallocate(mach_task_self(), cclock);
+#endif
+}
 
 int main() {
 #ifdef __MACH__
@@ -94,9 +129,14 @@ int main() {
         return 1;
     }
 
-    std::vector<uint32_t> our_ips;
+    std::vector<uint32_t> our_ips_big_endian;
+    std::vector<uint32_t> our_ips_little_endian;
+
     // We know file size and allocate that number of elements + some spare space
-    our_ips.reserve(200000);
+    our_ips_big_endian.reserve(200000);
+    our_ips_little_endian.reserve(200000);
+
+    
 
     while (getline(myfile, line)) {
         // It will be big endian internally
@@ -109,33 +149,77 @@ int main() {
             continue;
         }
 
-        our_ips.push_back(ip);
+        our_ips_big_endian.push_back(ip);
+
+        // Convert it to little endian
+        our_ips_little_endian.push_back(fast_ntoh(ip));
     }
 
 
-    std::cout << "Loaded " << our_ips.size() << " IPs into memory" << std::endl;
+    std::cout << "Loaded " << our_ips_big_endian.size() << " IPs into memory" << std::endl;
+
 
     {
-        std::cout << "std::map" << std::endl;
+        uint64_t accumulator = 0;
+        std::cout << std::endl << "std::map big endian " << std::endl;
 
         std::map<uint32_t, subnet_counter_t> std_map;
-        run_tests<std::map<uint32_t, subnet_counter_t>>(our_ips, std_map);
+        run_tests(our_ips_big_endian, std_map);
+        run_scan_tests(std_map, accumulator);
         std_map.clear();
     }
 
+
     {
-        std::cout << "std::unordered_map" << std::endl;
+        uint64_t accumulator = 0;
+        std::cout << std::endl << "std::map little endian " << std::endl;
+
+        std::map<uint32_t, subnet_counter_t> std_map;
+        run_tests(our_ips_little_endian, std_map);
+        run_scan_tests(std_map, accumulator);
+        std_map.clear();
+    }
+
+
+    {
+        uint64_t accumulator = 0;
+        std::cout << std::endl << "std::unordered_map big endian" << std::endl;
 
         std::unordered_map<uint32_t, subnet_counter_t> std_unordered;
-        run_tests<std::unordered_map<uint32_t, subnet_counter_t>>(our_ips, std_unordered);
+        run_tests(our_ips_big_endian, std_unordered);
+        run_scan_tests(std_unordered, accumulator);
         std_unordered.clear();
     }
 
     {
-        std::cout << "boost::unordered_map" << std::endl;
+        uint64_t accumulator = 0;
+        std::cout << std::endl << "std::unordered_map little endian" << std::endl;
+
+        std::unordered_map<uint32_t, subnet_counter_t> std_unordered;
+        run_tests(our_ips_little_endian, std_unordered);
+        run_scan_tests(std_unordered, accumulator);
+        std_unordered.clear();
+    }
+
+
+    {
+        uint64_t accumulator = 0;
+        std::cout << std::endl << "boost::unordered_map big endian " << std::endl;
 
         boost::unordered_map<uint32_t, subnet_counter_t> boost_unordered;
-        run_tests<boost::unordered_map<uint32_t, subnet_counter_t>>(our_ips, boost_unordered);
+        run_tests(our_ips_big_endian, boost_unordered);
+        run_scan_tests(boost_unordered, accumulator);
         boost_unordered.clear();
     }
+
+    {
+        uint64_t accumulator = 0;
+        std::cout << std::endl << "boost::unordered_map little endian " << std::endl;
+
+        boost::unordered_map<uint32_t, subnet_counter_t> boost_unordered;
+        run_tests(our_ips_little_endian, boost_unordered);
+        run_scan_tests(boost_unordered, accumulator);
+        boost_unordered.clear();
+    }
+
 }
