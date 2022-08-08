34d8f19f7fe3626a7491d2155e7affae9343471e
Pavel Odintsov <pavel.odintsov@gmail.com>
2022-08-08 16:31:13 +0100

    Fixed include path for Netmap plugin

--- src/netmap_plugin/netmap_collector.hpp
+++ src/netmap_plugin/netmap_collector.hpp
@@ -1,7 +1,7 @@
 #ifndef NETMAP_PLUGIN_H
 #define NETMAP_PLUGIN_H
 
-#include "../fastnetmon_types.h"
+#include "../fastnetmon_types.hpp"
 
 void start_netmap_collection(process_packet_pointer func_ptr);
 
