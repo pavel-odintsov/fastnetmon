e3e6811fbecdcf15f9cd4a41ac958199b18198d9
Pavel Odintsov <pavel.odintsov@gmail.com>
2022-08-08 16:40:41 +0100

    Removed old unified_parser logic

--- src/unified_parser.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "fastnetmon_types.hpp"
-#include <stdint.h>
-#include <sys/types.h>
-
-bool parse_raw_packet_to_simple_packet(u_char* buffer, int len, simple_packet_t& packet, bool netmap_read_packet_length_from_ip_header);
