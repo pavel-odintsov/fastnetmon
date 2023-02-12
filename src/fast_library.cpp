231443a443c53c0ae6a47330d8eee9d44cab9a2d
Pavel Odintsov <pavel.odintsov@gmail.com>
2023-02-12 20:57:29 +0000

    Improved IP address encoding and added direction encoding as enum

--- src/fast_library.cpp
+++ src/fast_library.cpp
@@ -2370,10 +2370,14 @@ bool write_simple_packet_to_protobuf(const simple_packet_t& packet, TrafficData&
     traffic_data.set_protocol(packet.protocol);
     traffic_data.set_sampling_ratio(packet.sample_ratio);
     
-    // packet.src_ipv6 and packet.dst_ipv6
-    traffic_data.set_source_ip(packet.src_ip);
-    traffic_data.set_destination_ip(packet.dst_ip);
-    
+    if (packet.ip_protocol_version == 4) {
+        traffic_data.set_source_ip(&packet.src_ip, sizeof(packet.src_ip));
+        traffic_data.set_destination_ip(&packet.dst_ip, sizeof(packet.dst_ip));
+    } else if (packet.ip_protocol_version == 6) {
+        traffic_data.set_source_ip(&packet.src_ipv6, sizeof(packet.src_ipv6));
+        traffic_data.set_destination_ip(&packet.dst_ipv6, sizeof(packet.dst_ipv6));
+    }
+
     traffic_data.set_ip_version(packet.ip_protocol_version);
     traffic_data.set_ttl(packet.ttl);
 
@@ -2388,13 +2392,27 @@ bool write_simple_packet_to_protobuf(const simple_packet_t& packet, TrafficData&
     traffic_data.set_timestamp_seconds(packet.ts.tv_sec);
     traffic_data.set_timestamp_milliseconds(packet.ts.tv_usec);
     traffic_data.set_octets(packet.length);
-    traffic_data.set_traffic_direction(packet.packet_direction);
-    traffic_data.set_source(packet.source);
+    
+    if (packet.packet_direction == INCOMING) {
+        traffic_data.set_traffic_direction(TRAFFIC_DIRECTION_INCOMING);
+    } else if (packet.packet_direction == OUTGOING) {
+        traffic_data.set_traffic_direction(TRAFFIC_DIRECTION_OUTGOING);
+    } else if (packet.packet_direction == INTERNAL) {
+        traffic_data.set_traffic_direction(TRAFFIC_DIRECTION_INTERNAL);
+    } else if (packet.packet_direction == OTHER) {
+        traffic_data.set_traffic_direction(TRAFFIC_DIRECTION_OTHER);
+    } else {
+        traffic_data.set_traffic_direction(TRAFFIC_DIRECTION_UNKNOWN);
+    }
+
+    traffic_data.set_telemetry_type(packet.source);
     traffic_data.set_source_asn(packet.src_asn);
     traffic_data.set_destination_asn(packet.dst_asn);
     traffic_data.set_input_interface(packet.input_interface);
     traffic_data.set_output_interface(packet.output_interface);
-    traffic_data.set_agent_address(packet.agent_ip_address);
+
+    // In current version we support only IPv4 agent IP
+    traffic_data.set_agent_address(&packet.agent_ip_address, sizeof(packet.agent_ip_address));
 
     return true;
 }
