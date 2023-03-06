c220afea845c1b5b0c993575a7d71a80d0ce7182
Babak Farrokhi <118838+farrokhi@users.noreply.github.com>
2023-03-06 14:06:34 +0100

    Update FreeBSD port to 1.2.4 (#971)
    Available on FreeBSD ports tree and can be installed via ports or pkg.

---
+++ src/packaging/FreeBSD/files/patch-src_notify__about__attack.sh
@@ -0,0 +1,8 @@
+--- src/notify_about_attack.sh.orig	2023-03-06 10:33:26 UTC
++++ src/notify_about_attack.sh
+@@ -1,4 +1,4 @@
+-#!/usr/bin/env bash
++#!/bin/sh
+ 
+ #
+ # Hello, lovely FastNetMon customer. I'm really happy to see you here
