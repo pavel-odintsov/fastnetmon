0e0153bb4c503727925362e6b0d9a43b9941c69a
Babak Farrokhi <babak@farrokhi.net>
2023-03-06 11:36:00 +0100

    Unbreak build on FreeBSD

--- src/fast_library.cpp
+++ src/fast_library.cpp
@@ -1249,7 +1249,12 @@ bool get_interface_number_by_device_name(int socket_fd, std::string interface_na
         return false;
     }
 
+#ifdef __FreeBSD__
+    interface_number = ifr.ifr_ifru.ifru_index;
+#else
     interface_number = ifr.ifr_ifindex;
+#endif
+
 #else
     /* Fallback to if_nametoindex(3) otherwise. */
     interface_number = if_nametoindex(interface_name.c_str());
