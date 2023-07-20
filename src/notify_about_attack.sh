665b9cbc7e4263e77156b85f928bf1edc5417080
Pavel Odintsov <pavel.odintsov@gmail.com>
2023-07-20 21:12:07 +0100

    Polished example callback script

--- src/notify_about_attack.sh
+++ src/notify_about_attack.sh
@@ -1,37 +1,33 @@
 #!/bin/sh
 
 #
-# Hello, lovely FastNetMon customer. I'm really happy to see you here
-#  Pavel Odintsov, author
+# This script will get following arguments from FastNetMon:
+#
+#  $1 IP of host which is under attack (incoming attack) or source of attack (outgoing attack)
+#  $2 Attack direction: incoming or outgoing
+#  $3 Attack bandwidth in packets per second
+#  $4 Attack action: ban or unban
 #
 
-# This script will get following params:
-#  $1 client_ip_as_string
-#  $2 data_direction
-#  $3 pps_as_string
-#  $4 action (ban or unban)
+email_notify="please_fix_this_email@domain.com"
 
-email_notify="root,please_fix_this_email@domain.ru"
-
-#
-# Please be careful. You should not remove cat >
-#
-
-if [ "$4" = "unban" ]; then
-    # No details arrived to stdin here
-
-    # Unban actions if used
-    exit 0
-fi
-
-#
 # For ban and attack_details actions we will receive attack details to stdin
 # Please do not remove "cat" command because
 # FastNetMon will crash in this case as it expects read of data from script side
 #
 
 if [ "$4" = "ban" ]; then
+    # This action receives multiple statistics about attack's performance and attack's sample to stdin
+
     cat | mail -s "FastNetMon Guard: IP $1 blocked because $2 attack with power $3 pps" $email_notify;
-    # You can add ban code here
+    
+    # Please add actions to run when we ban host
+    exit 0
+fi
+
+if [ "$4" = "unban" ]; then
+    # No details provided to stdin here
+
+    # Please add actions to run when we unban host
     exit 0
 fi
