bdf01adf5b990c0c92e062c64367b2537311ad39
Pavel Odintsov <pavel.odintsov@gmail.com>
2024-07-10 14:45:35 +0300

    Removed unused declarations

--- src/metrics/clickhouse.hpp
+++ src/metrics/clickhouse.hpp
@@ -11,6 +11,5 @@ bool push_network_traffic_counters_to_clickhouse();
 bool push_total_traffic_counters_to_clickhouse();
 bool push_hosts_traffic_counters_to_clickhouse();
 
-void create_clickhouse_attack_event(const std::string& ip_address, const std::string& title, const std::string& text);
 bool init_clickhouse_for_metrics(fastnetmon_configuration_t& fastnetmon_global_configuration,
                                  clickhouse::Client*& clickhouse_metrics_client);
